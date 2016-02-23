3c43b0789c536a4650a595f90965d5b3d8f320a5
hrvojem <hmatijak@gmail.com>
2016-02-23 09:52:53 +0100

    package version wasn't checked when packages were listed

--- psmdb_package_test.sh
+++ psmdb_package_test.sh
@@ -8,11 +8,11 @@ set -e
 
 echo "checking if all packages are installed"
 if [ -f /etc/redhat-release ]; then
-        if [ "$(rpm -qa | grep Percona | grep -c ${version})" = "6" ]; then
+        if [ "$(rpm -qa | grep Percona | grep -c ${version})" == "6" ]; then
                 echo "all packages are installed"
         else
                 for package in Percona-Server-MongoDB-debuginfo Percona-Server-MongoDB Percona-Server-MongoDB-mongos Percona-Server-MongoDB-server Percona-Server-MongoDB-shell Percona-Server-MongoDB-tools; do
-                        if [ "$(rpm -qa | grep -c ${package})" -gt 0 ]; then
+                        if [ "$(rpm -qa | grep -c ${package}-${version})" -gt 0 ]; then
                                 echo "$(date +%Y%m%d%H%M%S): ${package} is installed" >> ${log}
                         else
                                 echo "WARNING: ${package} is not installed"
@@ -20,11 +20,11 @@ if [ -f /etc/redhat-release ]; then
                 done
         fi
 else
-        if [ "$(dpkg -l | grep percona | grep -c ${version}) " = "6" ]; then
+        if [ "$(dpkg -l | grep percona | grep -c ${version})" == "6" ]; then
                 echo "all packages are installed"
         else
                 for package in percona-server-mongodb percona-server-mongodb-dbg percona-server-mongodb-mongos percona-server-mongodb-server percona-server-mongodb-shell percona-server-mongodb-tools; do
-                        if [ "$(dpkg -l | grep -c ${package})" -gt 0 ]; then
+                        if [ "$(dpkg -l | grep -c ${package}-${version})" -gt 0 ]; then
                                 echo "$(date +%Y%m%d%H%M%S): ${package} is installed" >> ${log}
                         else
                                 echo "WARNING: ${package} is not installed"
