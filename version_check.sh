e72fad6c2e6708477db81bbb47c1945ca695ad9b
Tomislav Plavcic <tomislav.plavcic@percona.com>
2016-07-08 11:11:52 +0200

    Added VERSIONS file, updated version_check.sh and added to common_57 playbook

--- version_check.sh
+++ version_check.sh
@@ -2,9 +2,32 @@
 
 set -e
 
-version="5.7.13-6"
-release="6"
-revision="e3d58bb"
+if [ "$#" -ne 1 ]; then
+  echo "This script requires product parameter: ps55, ps56 or ps57 !"
+  echo "Usage: ./version_check.sh <prod>"
+  exit 1
+fi
+
+SCRIPT_PWD=$(cd `dirname $0` && pwd)
+
+source ${SCRIPT_PWD}/VERSIONS
+
+if [ $1 = "ps55" ]; then
+  version=${PS55_VER}
+  release=${PS55_VER#*-}
+  revision=${PS55_REV}
+elif [ $1 = "ps56" ]; then
+  version=${PS56_VER}
+  release=${PS56_VER#*-}
+  revision=${PS56_REV}
+elif [ $1 = "ps57" ]; then
+  version=${PS57_VER}
+  release=${PS57_VER#*-}
+  revision=${PS57_REV}
+else
+  echo "Illegal product selected!"
+  exit 1
+fi
 
 log="/tmp/version_check.log"
 echo -n > $log
