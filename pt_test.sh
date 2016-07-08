223eb54889eedb1c856fac684a72321f28e661df
Tomislav Plavcic <tomislav.plavcic@percona.com>
2016-07-08 17:45:34 +0200

    Moved version check for PT and PXB under version_check.sh and added in to common_57 and common_57_upgrade playbooks

--- pt_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in `cat /vagrant/pt`; do $i --version; done
--- version_check.sh
+++ version_check.sh
@@ -24,28 +24,55 @@ elif [ $1 = "ps57" ]; then
   version=${PS57_VER}
   release=${PS57_VER#*-}
   revision=${PS57_REV}
+elif [ $1 = "pt" ]; then
+  version=${PT_VER}
+elif [ $1 = "pxb23" ]; then
+  version=${PXB23_VER}
+elif [ $1 = "pxb24" ]; then
+  version=${PXB24_VER}
 else
   echo "Illegal product selected!"
   exit 1
 fi
 
-log="/tmp/version_check.log"
+product=$1
+log="/tmp/${product}_version_check.log"
 echo -n > $log
 
-for i in @@INNODB_VERSION @@VERSION @@TOKUDB_VERSION; do
-        if [ "$(mysql -e "SELECT $i; "| grep -c $version)" = 1 ]; then
-                echo "$i is correct" >> $log
-        else
-                echo "@@INNODB_VERSION is incorrect"
-                exit 1
-        fi
-done
+if [ ${product} = "ps55" -o ${product} = "ps56" -o ${product} = "ps57" ]; then
+  for i in @@INNODB_VERSION @@VERSION @@TOKUDB_VERSION; do
+    if [ "$(mysql -e "SELECT $i; "| grep -c $version)" = 1 ]; then
+      echo "$i is correct" >> $log
+    else
+      echo "@@INNODB_VERSION is incorrect"
+      exit 1
+    fi
+  done
 
-if [ "$(mysql -e "SELECT @@VERSION_COMMENT;" | grep $revision | grep -c $release)" = 1 ]; then
-        echo "@@VERSION COMMENT is correct" >> $log
-else
-        echo "@@VERSION_COMMENT is incorrect"
-        exit 1
+  if [ "$(mysql -e "SELECT @@VERSION_COMMENT;" | grep $revision | grep -c $release)" = 1 ]; then
+    echo "@@VERSION COMMENT is correct" >> $log
+  else
+    echo "@@VERSION_COMMENT is incorrect"
+    exit 1
+  fi
+elif [ ${product} = "pt" ]; then
+  for i in `cat /package-testing/pt`; do
+    version_check=$($i --version|grep -c ${version})
+    if [ ${version_check} -eq 0 ]; then
+      echo "$i version is not good!"
+      exit 1
+    else
+      echo "$i version is correct and ${version}" >> $log
+    fi
+  done
+elif [ ${product} = "pxb23" -o ${product} = "pxb24" ]; then
+  version_check=$(xtrabackup --version|grep -c ${version})
+    if [ ${version_check} -eq 0 ]; then
+      echo "$i version is not good!"
+      exit 1
+    else
+      echo "${product} version is correct and ${version}" >> $log
+    fi
 fi
        
-echo "versions are OK"
+echo "${product} versions are OK"
