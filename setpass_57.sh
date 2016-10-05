147a151339f7098c94b7a7945d7c229e9ebfa902
Tomislav Plavcic <tomislav.plavcic@percona.com>
2016-10-05 11:59:39 +0200

    Small fix

--- setpass_57.sh
+++ setpass_57.sh
@@ -3,5 +3,5 @@ set -e
 
 init_pass=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
 
-mysql --connect-expired-password -uroot -p$init_pass -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'U?fY)9s7|3gxUm';"
+mysql --connect-expired-password -uroot --password="$init_pass" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'U?fY)9s7|3gxUm';"
 
