933cd58234ec08c383da6f84c1a5977b88490712
Tomislav Plavcic <tomislav.plavcic@percona.com>
2017-06-19 15:27:16 +0200

    Added check for new warnings and errors in error log when installing plugins and testing TokuDB compression libraries

--- plugins_test_55.sh
+++ plugins_test_55.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e
+WARNINGS_BEFORE=0
+WARNINGS_AFTER=0
+ERROR_LOG=""
+ERROR_LOG=$(mysql -N -s -e "show variables like 'log_error';" | grep -v "Warning:" | grep -o "\/.*$")
+if [ ! -f /var/log/mysql/error.log ]; then
+  echo "Error log was not found!"
+  exit 1
+fi
+WARNINGS_BEFORE=$(grep -c "\[Warning\]" ${ERROR_LOG})
+
 mysql -e "CREATE FUNCTION fnv1a_64 RETURNS INTEGER SONAME 'libfnv1a_udf.so'"
 mysql -e "CREATE FUNCTION fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so'"
 mysql -e "CREATE FUNCTION murmur_hash RETURNS INTEGER SONAME 'libmurmur_udf.so'"
@@ -11,3 +21,14 @@ mysql -e "SHOW PLUGINS;"
 mysql -e "CREATE DATABASE world;"
 sed -i '18,21 s/^/-- /' /package-testing/world.sql
 pv /package-testing/world.sql | mysql -D world
+
+if [ "$(grep -c "\[ERROR\]" ${ERROR_LOG})" != "0" ]; then
+  echo "There's an error in error log!"
+  exit 1
+fi
+
+WARNINGS_AFTER=$(grep -c "\[Warning\]" ${ERROR_LOG})
+if [ "${WARNINGS_BEFORE}" == "${WARNINGS_AFTER}" ]; then
+  echo "There's a difference in number of warnings before installing plugins and after!"
+  exit 1
+fi
--- plugins_test_56.sh
+++ plugins_test_56.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e
+WARNINGS_BEFORE=0
+WARNINGS_AFTER=0
+ERROR_LOG=""
+ERROR_LOG=$(mysql -N -s -e "show variables like 'log_error';" | grep -v "Warning:" | grep -o "\/.*$")
+if [ ! -f /var/log/mysql/error.log ]; then
+  echo "Error log was not found!"
+  exit 1
+fi
+WARNINGS_BEFORE=$(grep -c "\[Warning\]" ${ERROR_LOG})
+
 mysql -e "CREATE FUNCTION fnv1a_64 RETURNS INTEGER SONAME 'libfnv1a_udf.so'"
 mysql -e "CREATE FUNCTION fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so'"
 mysql -e "CREATE FUNCTION murmur_hash RETURNS INTEGER SONAME 'libmurmur_udf.so'"
@@ -21,4 +31,15 @@ if [ ! -z "$1" ]; then
     pv /package-testing/world.sql | mysql -D world2
     mysql < /package-testing/tokudb_compression.sql
   fi
+fi
+
+if [ "$(grep -c "\[ERROR\]" ${ERROR_LOG})" != "0" ]; then
+  echo "There's an error in error log!"
+  exit 1
+fi
+
+WARNINGS_AFTER=$(grep -c "\[Warning\]" ${ERROR_LOG})
+if [ "${WARNINGS_BEFORE}" == "${WARNINGS_AFTER}" ]; then
+  echo "There's a difference in number of warnings before installing plugins and after!"
+  exit 1
 fi
