fe41455c6c0ad75199eda953e87ae1d18b524138
Tomislav Plavcic <tomislav.plavcic@percona.com>
2017-06-29 13:52:37 +0200

    Add check_running script for checking if process is running in ansible

---
+++ check_running.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+  echo "This script requires product parameter: mysql, mongodb, proxysql and status like running|stopped!"
+  echo "Usage: ./check_running.sh <prod> <status>"
+  exit 1
+fi
+
+product=$1
+status=$2
+log="/tmp/${product}_check_running.log"
+echo -n > ${log}
+
+if [ ${product} = "mysql" ]; then
+  process="mysqld"
+elif [ ${product} = "mongodb" ]; then
+  process="mongod"
+elif [ ${product} = "proxysql" ]; then
+  process="proxysql"
+else
+  echo "Unknown product!"
+  exit 1
+fi
+
+ps aux >> ${log}
+psoutput=$(ps aux | grep -v grep | grep -c "${process}")
+echo ${psoutput}
+
+if [ ${psoutput} -eq 0 -a ${status} = "stopped" ]; then
+  exit 0
+elif [ ${psoutput} -gt 0 -a ${status} = "running" ]; then
+  exit 0
+else
+  exit 1
+fi
