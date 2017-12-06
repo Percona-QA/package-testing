65e0215c795b04c9fc15d5aca3b857fcf69142cd
hrvojem <hmatijak@gmail.com>
2017-12-06 09:40:09 +0100

    Counting errors before and after upgrade

--- plugins_test_55.sh
+++ plugins_test_55.sh
@@ -2,6 +2,8 @@
 set -e
 WARNINGS_BEFORE=0
 WARNINGS_AFTER=0
+ERRORS_BEFORE=0
+ERRORS_AFTER=0
 ERROR_LOG=""
 ERROR_LOG=$(mysql -N -s -e "show variables like 'log_error';" | grep -v "Warning:" | grep -o "\/.*$")
 if [ ! -f ${ERROR_LOG} ]; then
@@ -9,6 +11,7 @@ if [ ! -f ${ERROR_LOG} ]; then
   exit 1
 fi
 WARNINGS_BEFORE=$(grep -c "\[Warning\]" ${ERROR_LOG} || true)
+ERRORS_BEFORE=$(grep -c "\[ERROR\]" ${ERROR_LOG} || true)
 
 mysql -e "CREATE FUNCTION fnv1a_64 RETURNS INTEGER SONAME 'libfnv1a_udf.so'"
 mysql -e "CREATE FUNCTION fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so'"
@@ -22,8 +25,9 @@ mysql -e "CREATE DATABASE world;"
 sed -i '18,21 s/^/-- /' /package-testing/world.sql
 pv /package-testing/world.sql | mysql -D world
 
-if [ "$(grep -c "\[ERROR\]" ${ERROR_LOG} || true)" != "0" ]; then
-  echo "There's an error in error log!"
+ERRORS_AFTER=$(grep -c "\[ERROR\]" ${ERROR_LOG} || true)
+if [ "${ERRORS_BEFORE}" != "${ERRORS_AFTER}" ]; then
+  echo "There's a difference in number of errors before installing plugins and after!"
   exit 1
 fi
 
