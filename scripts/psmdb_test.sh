a20e516cc90786508fa0ee4d811c962e76c302ce
Tomislav Plavcic <tplavcic@gmail.com>
2018-10-04 14:33:09 +0200

    Improve hotbackup test for PSMDB and cleanup the psmdb_test.sh script

--- scripts/psmdb_test.sh
+++ scripts/psmdb_test.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-log="/tmp/psmdb_run.log"
-echo -n > /tmp/psmdb_run.log
+DATADIR=""
+BACKUPDIR="/tmp/backup"
+CONFIGFILE="/etc/mongod.conf"
+
+LOG="/tmp/psmdb_run.log"
+echo -n > ${LOG}
 
 source /package-testing/VERSIONS
 
 SLES=0
 if [ -f /etc/os-release ]; then
   SLES=$(cat /etc/os-release | grep -c '^NAME=\"SLES' || true)
+fi
+
+if [ -f /etc/redhat-release -o ${SLES} -eq 1 ]; then
+  DATADIR="/var/lib/mongo"
+else
+  DATADIR="/var/lib/mongodb"
 fi
 
 set -e
@@ -25,16 +35,16 @@ fi
 # Enable auditLog and profiling/rate limit to see if services start with those
 if [ "$VERSION" == "3.0" ]; then
   echo "Skipping usage of profiling rate limit functionality because not available in 3.0"
-  sed -i 's/#operationProfiling:/operationProfiling:\n  mode: all\n  slowOpThresholdMs: 200/' /etc/mongod.conf
+  sed -i 's/#operationProfiling:/operationProfiling:\n  mode: all\n  slowOpThresholdMs: 200/' ${CONFIGFILE}
 else
-  sed -i 's/#operationProfiling:/operationProfiling:\n  mode: all\n  slowOpThresholdMs: 200\n  rateLimit: 100/' /etc/mongod.conf
+  sed -i 's/#operationProfiling:/operationProfiling:\n  mode: all\n  slowOpThresholdMs: 200\n  rateLimit: 100/' ${CONFIGFILE}
 fi
-sed -i 's/#auditLog:/audit:\n  destination: file\n  path: \/tmp\/audit.json/' /etc/mongod.conf
+sed -i 's/#auditLog:/audit:\n  destination: file\n  path: \/tmp\/audit.json/' ${CONFIGFILE}
 
 # Enable --useDeprecatedMongoRocks for 3.6 to be able to start service with mongorocks
 if [ "$VERSION" == "3.6" ]; then
-  echo "Adding --useDeprecatedMongoRocks option to mongod.cnf" 
-  sed -i '/engine: rocksdb/a \  useDeprecatedMongoRocks: true' /etc/mongod.conf
+  echo "Adding --useDeprecatedMongoRocks option to mongod.cnf"
+  sed -i '/engine: rocksdb/a \  useDeprecatedMongoRocks: true' ${CONFIGFILE}
 fi
 
 function start_service {
@@ -84,44 +94,42 @@ function stop_service {
 }
 
 function list_data {
-  if [ -f /etc/redhat-release -o ${SLES} -eq 1 ]; then
-    echo "$(date +%Y%m%d%H%M%S): contents of the mongo data dir: " >> $log
-    ls -alh /var/lib/mongo/ >> $log
-  else
-    echo "$(date +%Y%m%d%H%M%S): contents of the mongodb data dir: " >> $log
-    ls -alh /var/lib/mongodb/ >> $log
-  fi
+  echo -e "listing files in datadir...\n"
+  ls -alh ${DATADIR}/ >> ${LOG}
 }
 
 function clean_datadir {
-  if [ -f /etc/redhat-release -o ${SLES} -eq 1 ]; then
-    echo -e "removing the data files (on rhel distros)...\n"
-    rm -rf /var/lib/mongo/*
-  else
-    echo -e "removing the data files (on debian distros)...\n"
-    rm -rf /var/lib/mongodb/*
-  fi
+  echo -e "removing the data files...\n"
+  rm -rf ${DATADIR}/*
 }
 
 function test_hotbackup {
-  rm -rf /tmp/backup
-  mkdir -p /tmp/backup
-  chown mongod:mongod -R /tmp/backup
-  BACKUP_RET=$(mongo admin --eval "$(cat ~/.mongorc.js); db.runCommand({createBackup: 1, backupDir: '/tmp/backup'})"|grep -c '"ok" : 1')
-  rm -rf /tmp/backup
+  MD5_BEFORE=$(mongo localhost:27017/test --quiet --eval "db.runCommand({ dbHash: 1 }).md5" | tail -n1)
+  rm -rf ${BACKUPDIR}
+  mkdir -p ${BACKUPDIR}
+  chown mongod:mongod -R ${BACKUPDIR}
+  BACKUP_RET=$(mongo admin --eval "$(cat ~/.mongorc.js); db.runCommand({createBackup: 1, backupDir: '${BACKUPDIR}'})"|grep -c '"ok" : 1')
   if [ ${BACKUP_RET} = 0 ]; then
-    echo "Backup failed for storage engine: ${engine}"
+    echo "Backup failed for storage engine: ${engine}" | tee -a ${LOG}
     exit 1
   fi
+  stop_service
+  clean_datadir
+  cp -r ${BACKUPDIR}/* ${DATADIR}/
+  chown -R mongod:mongod ${DATADIR}
+  start_service
+  MD5_AFTER=$(mongo localhost:27017/test --quiet --eval "db.runCommand({ dbHash: 1 }).md5" | tail -n1)
+  if [ "${MD5_BEFORE}" != "${MD5_AFTER}" ]; then
+    echo "ERROR: dbHash before and after hotbackup are not the same!" | tee -a ${LOG}
+    exit 1
+  else
+    echo "dbHash is the same before and after hotbackup: ${MD5_BEFORE}:${MD5_AFTER}" | tee -a ${LOG}
+  fi
+  rm -rf ${BACKUPDIR}
 }
 
 function check_rocksdb {
-  ROCKSDB_LOG_FILE=""
-  if [ -f /etc/redhat-release -o ${SLES} -eq 1 ]; then
-    ROCKSDB_LOG_FILE="/var/lib/mongo/db/LOG"
-  else
-    ROCKSDB_LOG_FILE="/var/lib/mongodb/db/LOG"
-  fi
+  ROCKSDB_LOG_FILE="${DATADIR}/db/LOG"
   # Check RocksDB library version
   ROCKSDB_VERSION=$(grep "RocksDB version" ${ROCKSDB_LOG_FILE}|tail -n1|grep -Eo "[0-9]+\.[0-9]+(\.[0-9]+)*$")
   if [ "${VERSION}" == "3.0" ]; then
@@ -168,25 +176,25 @@ for engine in mmapv1 PerconaFT rocksdb wiredTiger inMemory; do
   else
     stop_service
     clean_datadir
-    echo "=================" | tee -a $log
-    echo "testing ${engine}" | tee -a $log
-    echo "=================" | tee -a $log
-    sed -i "/engine: *${engine}/s/#//g" /etc/mongod.conf
+    echo "=================" | tee -a ${LOG}
+    echo "testing ${engine}" | tee -a ${LOG}
+    echo "=================" | tee -a ${LOG}
+    sed -i "/engine: *${engine}/s/#//g" ${CONFIGFILE}
     start_service
-    mongo --eval "db.serverStatus().storageEngine" | tee -a $log
+    mongo --eval "db.serverStatus().storageEngine" | tee -a ${LOG}
     if [ ${engine} == "rocksdb" ]; then
       check_rocksdb
     fi
     echo "importing the sample data"
-    mongo < /package-testing/scripts/mongo_insert.js >> $log
-    list_data >> $log
+    mongo < /package-testing/scripts/mongo_insert.js >> ${LOG}
+    list_data >> ${LOG}
     if [[ ${engine} = "wiredTiger" || ${engine} = "rocksdb" ]] && [[ "$1" != "3.0" ]]; then
       echo "testing the hotbackup functionality"
       test_hotbackup
     fi
     stop_service
     echo "disable ${engine}"
-    sed -i "/engine: *${engine}/s//#engine: ${engine}/g" /etc/mongod.conf
+    sed -i "/engine: *${engine}/s//#engine: ${engine}/g" ${CONFIGFILE}
     clean_datadir
   fi
   start_service
@@ -194,33 +202,35 @@ done
 
 if [ "$1" != "3.4" ]; then
   for cipher in AES256-CBC AES256-GCM; do
-    echo "preparing datadir for testing encryption with ${cipher}"
+    echo "==================================" | tee -a ${LOG}
+    echo "testing encryption with ${cipher}" | tee -a ${LOG}
+    echo "==================================" | tee -a ${LOG}
+    echo "preparing datadir for testing encryption with ${cipher}" | tee -a ${LOG}
     stop_service
     clean_datadir
-    sed -i "/^  engine: /s/^/#/g" /etc/mongod.conf
+    sed -i "/^  engine: /s/^/#/g" ${CONFIGFILE}
     engine="wiredTiger"
-    sed -i "/engine: *${engine}/s/#//g" /etc/mongod.conf
-    echo "testing encryption with ${cipher}"
+    sed -i "/engine: *${engine}/s/#//g" ${CONFIGFILE}
     if [ ${cipher} = "AES256-CBC" ]; then
-      sed -i "s/#security:/security:\n  enableEncryption: true\n  encryptionCipherMode: ${cipher}\n  encryptionKeyFile: \/package-testing\/scripts\/mongodb-keyfile/" /etc/mongod.conf
+      sed -i "s/#security:/security:\n  enableEncryption: true\n  encryptionCipherMode: ${cipher}\n  encryptionKeyFile: \/package-testing\/scripts\/mongodb-keyfile/" ${CONFIGFILE}
     else
-      sed -i "s/encryptionCipherMode: AES256-CBC/encryptionCipherMode: AES256-GCM/" /etc/mongod.conf
+      sed -i "s/encryptionCipherMode: AES256-CBC/encryptionCipherMode: AES256-GCM/" ${CONFIGFILE}
     fi
     start_service
     if [ "$(mongo --quiet --eval "db.serverCmdLineOpts().parsed.security.enableEncryption" | tail -n1)" != "true" ]; then
-      echo "ERROR: Encryption is not enabled!"
+      echo "ERROR: Encryption is not enabled!" | tee -a ${LOG}
       exit 1
     elif [ "$(mongo --quiet --eval "db.serverCmdLineOpts().parsed.security.encryptionCipherMode" | tail -n1)" != "${cipher}" ]; then
-      echo "ERROR: Cipher mode is not set to: ${cipher}"
+      echo "ERROR: Cipher mode is not set to: ${cipher}" | tee -a ${LOG}
       exit 1
     elif [ "$(mongo --quiet --eval "db.serverCmdLineOpts().parsed.security.encryptionKeyFile" | tail -n1)" != "/package-testing/scripts/mongodb-keyfile" ]; then
-      echo "ERROR: Encryption key file is not set to: /package-testing/scripts/mongodb-keyfile"
+      echo "ERROR: Encryption key file is not set to: /package-testing/scripts/mongodb-keyfile" | tee -a ${LOG}
       exit 1
     fi
-    echo "adding some data and indexes with cipher ${cipher}"
-    mongo localhost:27017/test --eval "for(i=1; i <= 100000; i++) { db.series.insert( { id: i, name: 'series'+i })}"
-    mongo localhost:27017/test --eval "db.series.ensureIndex({ name: 1 })"
-    echo "testing the hotbackup functionality with ${cipher}"
+    echo "adding some data and indexes with cipher ${cipher}" | tee -a ${LOG}
+    mongo localhost:27017/test --eval "for(i=1; i <= 100000; i++) { db.series.insert( { id: i, name: 'series'+i })}" >> ${LOG}
+    mongo localhost:27017/test --eval "db.series.ensureIndex({ name: 1 })" >> ${LOG}
+    echo "testing the hotbackup functionality with ${cipher}" | tee -a ${LOG}
     test_hotbackup
   done
 fi
