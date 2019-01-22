d8e1e95163bda3f53aebaa2e2efbc3cc56a760e8
hrvojem <hmatijak@gmail.com>
2019-01-22 18:20:14 +0100

    Copy the config from template when resetting the password

--- setpass_57.sh
+++ setpass_57.sh
@@ -5,3 +5,4 @@ init_pass=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
 
 mysql --connect-expired-password -uroot --password="$init_pass" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'U?fY)9s7|3gxUm';"
 
+cp /vagrant/templates/my_57.j2 /root/.my.cnf
