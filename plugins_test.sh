4404fa1fe570a4072117b636babf208cde5a8d24
hrvojem <hmatijak@gmail.com>
2019-02-27 15:50:44 +0100

    Additional pxb playbooks

--- plugins_test.sh
+++ plugins_test.sh
@@ -3,5 +3,6 @@ mysql -e "CREATE FUNCTION fnv1a_64 RETURNS INTEGER SONAME 'libfnv1a_udf.so'"
 mysql -e "CREATE FUNCTION fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so'"
 mysql -e "CREATE FUNCTION murmur_hash RETURNS INTEGER SONAME 'libmurmur_udf.so'"
 mysql -e "CREATE DATABASE world;"
+mysql -e "CREATE DATABASE sbt;"
 sed -i '18,21 s/^/-- /' /package-testing/world.sql
 pv /package-testing/world.sql | mysql -D world
