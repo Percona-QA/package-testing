4483248a007c8267ac6a58e52ed4a9a162c79433
hrvojem <hmatijak@gmail.com>
2019-02-27 16:12:42 +0100

    Add another database

--- plugins_test.sh
+++ plugins_test.sh
@@ -4,5 +4,6 @@ mysql -e "CREATE FUNCTION fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so'"
 mysql -e "CREATE FUNCTION murmur_hash RETURNS INTEGER SONAME 'libmurmur_udf.so'"
 mysql -e "CREATE DATABASE world;"
 mysql -e "CREATE DATABASE sbt;"
+mysql -e "CREATE DATABASE sb;"
 sed -i '18,21 s/^/-- /' /package-testing/world.sql
 pv /package-testing/world.sql | mysql -D world
