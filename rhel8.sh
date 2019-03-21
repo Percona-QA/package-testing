3f5725635fa78d94844769cb8aa54d64665f862d
hrvojem <hmatijak@gmail.com>
2019-03-21 12:55:04 +0100

    RHEL8 fixes

---
+++ rhel8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This scripts preps the rhel8 box to be handled with ansible
+
+if [ -f /etc/redhat-release ] && [ "$(grep -c Red /etc/redhat-release)" == 1 ]; then
+  echo "This is RHEL8"
+  curl -o /etc/yum.repos.d/percona-dev.repo https://jenkins.percona.com/yum-repo/rhel8/rhel8-beta.repo
+  yum install python3 python3-dnf -y
+  ln -s /usr/bin/python3 /usr/bin/python
+else
+  echo "Not RHEL; nothing to do here"
+fi
