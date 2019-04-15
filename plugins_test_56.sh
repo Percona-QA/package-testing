b1039c9bcc2f3f581fdc84d52cd85271223dfb12
hrvojem <hmatijak@gmail.com>
2019-04-15 15:13:35 +0200

    Use cat instead of pv

--- plugins_test_56.sh
+++ plugins_test_56.sh
@@ -27,11 +27,11 @@ mysql -e "CREATE DATABASE sb;"
 # adding world database for SE tests
 mysql -e "CREATE DATABASE world;"
 sed -i '18,21 s/^/-- /' /package-testing/world.sql
-pv /package-testing/world.sql | mysql -D world
+cat /package-testing/world.sql | mysql -D world
 if [ ! -z "$1" ]; then
   if [ "$1" = "ps" ]; then
     mysql -e "CREATE DATABASE world2;"
-    pv /package-testing/world.sql | mysql -D world2
+    cat /package-testing/world.sql | mysql -D world2
     mysql < /package-testing/tokudb_compression.sql
   fi
 fi
