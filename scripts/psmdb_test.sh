b8f010076d9bb07abf7f8bb88da52c5ec2b1dadd
Tomislav Plavcic <tplavcic@gmail.com>
2019-05-17 12:19:36 +0200

    Changes for testing PSMDB encryption

--- scripts/psmdb_test.sh
+++ scripts/psmdb_test.sh
@@ -203,8 +203,8 @@ for engine in mmapv1 PerconaFT rocksdb wiredTiger inMemory; do
 done
 
 if [ "$1" == "3.6" ] || [ "$1" == "4.0" ]; then
-  chmod 600 /package-testing/scripts/mongodb-keyfile
-  chown mongod:mongod /package-testing/scripts/mongodb-keyfile
+  chmod 600 /package-testing/scripts/psmdb_encryption/mongodb-keyfile
+  chown mongod:mongod /package-testing/scripts/psmdb_encryption/mongodb-keyfile
   for cipher in AES256-CBC AES256-GCM; do
     echo "==================================" | tee -a ${LOG}
     echo "testing encryption with ${cipher}" | tee -a ${LOG}
@@ -216,7 +216,7 @@ if [ "$1" == "3.6" ] || [ "$1" == "4.0" ]; then
     engine="wiredTiger"
     sed -i "/engine: *${engine}/s/#//g" ${CONFIGFILE}
     if [ ${cipher} = "AES256-CBC" ]; then
-      sed -i "s/#security:/security:\n  enableEncryption: true\n  encryptionCipherMode: ${cipher}\n  encryptionKeyFile: \/package-testing\/scripts\/mongodb-keyfile/" ${CONFIGFILE}
+      sed -i "s/#security:/security:\n  enableEncryption: true\n  encryptionCipherMode: ${cipher}\n  encryptionKeyFile: \/package-testing\/scripts\/psmdb_encryption\/mongodb-keyfile/" ${CONFIGFILE}
     else
       sed -i "s/encryptionCipherMode: AES256-CBC/encryptionCipherMode: AES256-GCM/" ${CONFIGFILE}
     fi
@@ -227,8 +227,8 @@ if [ "$1" == "3.6" ] || [ "$1" == "4.0" ]; then
     elif [ "$(mongo --quiet --eval "db.serverCmdLineOpts().parsed.security.encryptionCipherMode" | tail -n1)" != "${cipher}" ]; then
       echo "ERROR: Cipher mode is not set to: ${cipher}" | tee -a ${LOG}
       exit 1
-    elif [ "$(mongo --quiet --eval "db.serverCmdLineOpts().parsed.security.encryptionKeyFile" | tail -n1)" != "/package-testing/scripts/mongodb-keyfile" ]; then
-      echo "ERROR: Encryption key file is not set to: /package-testing/scripts/mongodb-keyfile" | tee -a ${LOG}
+    elif [ "$(mongo --quiet --eval "db.serverCmdLineOpts().parsed.security.encryptionKeyFile" | tail -n1)" != "/package-testing/scripts/psmdb_encryption/mongodb-keyfile" ]; then
+      echo "ERROR: Encryption key file is not set to: /package-testing/scripts/psmdb_encryption/mongodb-keyfile" | tee -a ${LOG}
       exit 1
     fi
     echo "adding some data and indexes with cipher ${cipher}" | tee -a ${LOG}
