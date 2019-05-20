c9ccb167dad7cdf6429a5debd920ba58cbe70128
hrvojem <hmatijak@gmail.com>
2019-05-20 13:37:45 +0200

    Replace pv with cat

--- plugins_test_57u.sh
+++ plugins_test_57u.sh
@@ -24,11 +24,11 @@ mysql -e "INSTALL PLUGIN mysqlx SONAME 'mysqlx.so';"
 mysql -e "SHOW PLUGINS;"
 mysql -e "CREATE DATABASE world;"
 sed -i '18,21 s/^/-- /' /package-testing/world.sql
-pv /package-testing/world.sql | mysql -D world
+cat /package-testing/world.sql | mysql -D world
 if [ ! -z "$1" ]; then
   if [ "$1" = "ps" ]; then
     mysql -e "CREATE DATABASE world2;"
-    pv /package-testing/world.sql | mysql -D world2
+    cat /package-testing/world.sql | mysql -D world2
     mysql < /package-testing/tokudb_compression.sql
   fi
 fi
