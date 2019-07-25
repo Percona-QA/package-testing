28789c4f75961755c884ecef26dafa0e6cc43d7f
Mikhail Samoylov <mikhail.samoylov@percona.com>
2019-07-25 19:39:15 +0300

    New tests for postgres and new platforms for testing.

--- molecule/pg-11/molecule/default/tests/test_pg.py
+++ molecule/pg-11/molecule/default/tests/test_pg.py
@@ -12,10 +12,26 @@ DEB_PACKAGES = ["percona-platform-postgresql-11", "percona-platform-postgresql-c
                 "percona-platform-postgresql-contrib", "percona-platform-postgresql-doc",
                 "percona-platform-postgresql-doc-11", "percona-platform-postgresql-plperl-11",
                 "percona-platform-postgresql-plpython-11", "percona-platform-postgresql-plpython3-11",
-                "percona-platform-postgresql-pltcl-11"]
+                "percona-platform-postgresql-pltcl-11", "percona-platform-postgresql-all"]
+
+RPM_PACKAGES = ["percona-platform-postgresql11", "percona-platform-postgresql11-contrib",
+                "percona-platform-postgresql11-debuginfo", "percona-platform-postgresql11-devel",
+                "percona-platform-postgresql11-docs", "percona-platform-postgresql11-libs",
+                "percona-platform-postgresql11-plperl", "percona-platform-postgresql11-plpython",
+                "percona-platform-postgresql11-pltcl", "percona-platform-postgresql11-server",
+                "percona-platform-postgresql11-test"]
 
 
 @pytest.mark.parametrize("package", DEB_PACKAGES)
+def test_deb_package_is_installed(host, package):
+    os = host.system_info.distribution
+    if os == "RedHat":
+        pytest.skip("This test only for Debian based platforms")
+    pkg = host.package(package)
+    assert pkg.is_installed
+
+
+@pytest.mark.parametrize("package", RPM_PACKAGES)
 def test_deb_package_is_installed(host, package):
     os = host.system_info.distribution
     if os == "RedHat":
@@ -28,8 +44,18 @@ def test_postgresql_is_running_and_enabled(host):
     if os == "RedHat":
         pytest.skip("This test only for Debian based platforms")
     postgresql = host.service("postgresql")
-    # assert postgresql.is_running
+    assert postgresql.is_running
     assert postgresql.is_enabled
+
+def test_postgres_binary(host):
+    pass
+
+def test_postgres_server_version(host):
+    pass
+
+def test_postgres_client_version(host):
+    pass
+
 
 # def test_command_output(host):
 #     if os == "RedHat":
