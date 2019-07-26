d6f2411c8f72d247b4a042208c866c43f868dbfc
Mikhail Samoylov <mikhail.samoylov@percona.com>
2019-07-26 22:36:39 +0300

    New tests for postgres.

--- molecule/pg-11/molecule/default/tests/test_pg.py
+++ molecule/pg-11/molecule/default/tests/test_pg.py
@@ -21,6 +21,8 @@ RPM_PACKAGES = ["percona-platform-postgresql11", "percona-platform-postgresql11-
                 "percona-platform-postgresql11-pltcl", "percona-platform-postgresql11-server",
                 "percona-platform-postgresql11-test"]
 
+EXTENSIONS = []
+
 
 @pytest.fixture()
 def postgres_unit_file(host):
@@ -41,6 +43,21 @@ def start_stop_postgresql(host):
 
 
 @pytest.fixture()
+def postgresql_binary(host):
+    os = host.system_info.distribution
+    if os == "RedHat":
+        return
+    elif os == "debian":
+        return host.file("/usr/lib/postgresql/11/bin/postgres")
+
+
+@pytest.fixture()
+def postgresql_query_version(host):
+    with host.sudo("postgres"):
+        return host.check_output("psql -c 'SELECT version()'")
+
+
+@pytest.fixture()
 def restart_postgresql(host):
     cmd = "sudo systemctl restart postgresql"
     result = host.run(cmd)
@@ -52,7 +69,6 @@ def restart_postgresql(host):
 @pytest.mark.parametrize("package", DEB_PACKAGES)
 def test_deb_package_is_installed(host, package):
     os = host.system_info.distribution
-    print(os)
     if os == "RedHat":
         pytest.skip("This test only for Debian based platforms")
     pkg = host.package(package)
@@ -62,7 +78,6 @@ def test_deb_package_is_installed(host, package):
 @pytest.mark.parametrize("package", RPM_PACKAGES)
 def test_rpm_package_is_installed(host, package):
     os = host.system_info.distribution
-    print(os)
     if os == "debian":
         pytest.skip("This test only for RHEL based platforms")
     pkg = host.package(package)
@@ -97,14 +112,23 @@ def test_postgres_unit_file(postgres_unit_file):
     assert "postgresql" in postgres_unit_file
 
 
-def test_postgres_binary(host):
-    pass
+def test_postgres_binary(postgresql_binary):
+    assert postgresql_binary.exists
+    assert postgresql_binary.user == "root"
 
 
-def test_postgres_server_version(host):
+def test_pg_config_server_version(host):
     cmd = "pg_config --version"
-    result = host.check_output(cmd)
-    assert "11" in result
+    try:
+        result = host.check_output(cmd)
+        assert "11" in result
+    except AssertionError:
+        pytest.mark.xfail(reason="Maybe dev package not install")
+
+
+def test_postgresql_query_version(postgresql_query_version):
+    assert postgresql_query_version.rc == 0
+    print(postgresql_query_version.stdout)
 
 
 def test_postgres_client_version(host):
@@ -123,4 +147,13 @@ def test_restart_postgresql(restart_postgresql):
     assert restart_postgresql.rc == 0
     assert "active" in restart_postgresql.stdout
     assert not restart_postgresql.stderr
+
+
+def test_extenstions_list(host):
+    pass
+
+
+@pytest.mark.parametrize("extension", EXTENSIONS)
+def test_enable_extension(host, extension):
+    pass
 
