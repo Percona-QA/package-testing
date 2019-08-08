fc3744aefebb37c56007ac21f4c99eefdefec9d7
Mikhail Samoylov <mikhail.samoylov@percona.com>
2019-08-08 12:56:46 +0300

    Fix.

--- molecule/pg-11/molecule/default/tests/test_tools.py
+++ molecule/pg-11/molecule/default/tests/test_tools.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import time
 
 import testinfra.utils.ansible_runner
 
@@ -17,6 +18,7 @@ def pgaudit(host):
             result = host.run(cmd)
             assert result.rc == 0
             print(result.stdout)
+            time.sleep(15)
             cmd = "/usr/pgsql-11/bin/pg_ctl -D /var/lib/pgsql/11/data/ start"
             result = host.run(cmd)
             print(result.stdout)
