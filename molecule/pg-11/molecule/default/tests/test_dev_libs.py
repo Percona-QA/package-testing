64d585bb1fd509fd03e694eaa48ec39bd1498c38
Mikhail Samoylov <mikhail.samoylov@percona.com>
2019-08-27 10:25:58 +0300

    Libpq test.

--- molecule/pg-11/molecule/default/tests/test_dev_libs.py
+++ molecule/pg-11/molecule/default/tests/test_dev_libs.py
@@ -13,6 +13,42 @@ PACKAGES = ["libecpg-compat3", "libecpg-compat3-dbgsym", "libecpg-dev-dbgsym", "
             'libecpg6', "libpgtypes3", "libpgtypes3-dbgsym", "libpq-dev", "libpq5-dbgsym", "libpq5"]
 
 
+# @pytest.fixture()
+# def fdw_extension(host):
+#     with host.sudo("postgres"):
+#         install_extension = host.run("psql -c 'CREATE EXTENSION \"postgres_fdw\";'")
+#         try:
+#             assert install_extension.rc == 0
+#             assert install_extension.stdout.strip("\n") == "CREATE EXTENSION"
+#         except AssertionError:
+#             pytest.fail("Return code {}. Stderror: {}. Stdout {}".format(install_extension.rc,
+#                                                                          install_extension.stderr,
+#                                                                          install_extension.stdout))
+#
+#         try:
+#             extensions = host.run("psql -c 'SELECT * FROM pg_extension;' | awk 'NR>=3{print $1}'")
+#             assert extensions.rc == 0
+#             assert "postgres_fdw" in set(extensions.stdout.split())
+#         except AssertionError:
+#             pytest.fail("Return code {}. Stderror: {}. Stdout {}").format(extensions.rc, extensions.stderr,
+#                                                                           extensions.stdout)
+#
+#
+# @pytest.fixture()
+# def fdw_functional(host):
+#     pass
+
+
+@pytest.fixture()
+def build_libpq_programm(host):
+    pg_include_cmd = "pg_config --includedir"
+    pg_include = host.check_output(pg_include_cmd)
+    lib_dir_cmd = "pg_config --libdir"
+    lib_dir = host.check_output(lib_dir_cmd)
+    print(lib_dir)
+    return host.run("gcc -o lib_version /tmp/libpq_command_temp_dir/lib_version.c -I{} -lpq -std=c99".format(pg_include)
+
+
 @pytest.mark.parametrize("package", PACKAGES)
 def test_deb_package_is_installed(host, package):
     os = host.system_info.distribution
@@ -21,3 +57,17 @@ def test_deb_package_is_installed(host, package):
     pkg = host.package(package)
     assert pkg.is_installed
     assert pkg.version in DEB_PKG_VERSIONS
+
+
+def test_build_libpq_programm(build_libpq_programm):
+    print(build_libpq_programm.stdout)
+    print(build_libpq_programm.stderr)
+
+
+
+# def test_fdw_extenstion(fdw_extension):
+#     pass
+#
+#
+# def test_fdw_functional(fdw_functional):
+#     pass
