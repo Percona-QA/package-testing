0a0fe2e7c627f7e7caa54829865bf31725bb0991
Tomislav Plavcic <tplavcic@gmail.com>
2019-09-03 17:29:27 +0200

    Test for rhel8

--- rhel8.sh
+++ rhel8.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # This scripts preps the rhel8 box to be handled with ansible
 
 if [ -f /etc/redhat-release ] && [ "$(grep -c Red /etc/redhat-release)" == 1 ]; then
   echo "This is RHEL8"
+  subscription-manager register --username=$1 --password=$2 --auto-attach
   curl -o /etc/yum.repos.d/percona-dev.repo https://jenkins.percona.com/yum-repo/rhel8/rhel8-beta.repo
   yum install python3 python3-dnf -y
   ln -s /usr/bin/python3 /usr/bin/python
