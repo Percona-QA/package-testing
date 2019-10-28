c66fdddaab0a6b212d6cd25ac408c8c2f94350c6
hrvojem <hmatijak@gmail.com>
2019-10-28 11:26:50 +0100

    Check that clone plugin is active

--- plugins_test_80.sh
+++ plugins_test_80.sh
@@ -35,6 +35,16 @@ mysql -e "INSTALL PLUGIN data_masking SONAME 'data_masking.so';"
 mysql -e "INSTALL PLUGIN clone SONAME 'mysql_clone.so';"
 
 
+# make sure clone plugin is active
+CLONE_PLUGIN=$(mysql -e "SELECT PLUGIN_NAME, PLUGIN_STATUS FROM INFORMATION_SCHEMA.PLUGINS WHERE PLUGIN_NAME = 'clone';" | grep -c ACTIVE)
+
+if [ ${CLONE_PLUGIN} == 1 ]; then
+   echo "Clone plugin is installed and active"
+else
+   echo "ERROR: Clone plugin isn't installed or active"
+   exit 1
+fi
+
 mysql -e "SHOW PLUGINS;"
 mysql -e "CREATE DATABASE world;"
 sed -i '18,21 s/^/-- /' /package-testing/world.sql
