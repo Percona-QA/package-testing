bb9f4922a191cc668b8dc95fc0b79dab07956dc7
Tomislav Plavcic <tplavcic@gmail.com>
2020-02-25 16:43:36 +0100

    Update PXC docker test

--- docker-image-tests/pxc/tests/test_container_att.py
+++ docker-image-tests/pxc/tests/test_container_att.py
@@ -15,44 +15,44 @@ def inspect_data():
         ['docker', 'run', '--name', container_name, '-e', 'MYSQL_ROOT_PASSWORD='+pxc_pwd, '-d', docker_image]).decode().strip()
     # return json data with info about container
     inspect_data = json.loads(subprocess.check_output(['docker','inspect',container_name]))
-    yield inspect_data
+    yield inspect_data[0]
     # at the end of the test suite, destroy the container
     subprocess.check_call(['docker', 'rm', '-f', docker_id])
 
 
 class TestContainerAttributes:
     def test_args(self, inspect_data):
-        assert len(inspect_data[0]['Args']) == 1
-        assert inspect_data[0]['Args'][0] == 'mysqld'
+        assert len(inspect_data['Args']) == 1
+        assert inspect_data['Args'][0] == 'mysqld'
 
     def test_status(self, inspect_data):
-        assert inspect_data[0]['State']['Status'] == 'running'
-        assert inspect_data[0]['State']['Running'] == True
+        assert inspect_data['State']['Status'] == 'running'
+        assert inspect_data['State']['Running'] == True
 
     def test_config(self, inspect_data):
-        assert len(inspect_data[0]['Config']['Cmd']) == 1
-        assert inspect_data[0]['Config']['Cmd'][0] == 'mysqld'
+        assert len(inspect_data['Config']['Cmd']) == 1
+        assert inspect_data['Config']['Cmd'][0] == 'mysqld'
 
     def test_image_name(self, inspect_data):
-        assert inspect_data[0]['Config']['Image'] == docker_image
+        assert inspect_data['Config']['Image'] == docker_image
 
     def test_volumes(self, inspect_data):
-        assert len(inspect_data[0]['Config']['Volumes']) == 2
-        assert '/var/lib/mysql' in inspect_data[0]['Config']['Volumes']
-        assert '/var/log/mysql' in inspect_data[0]['Config']['Volumes']
+        assert len(inspect_data['Config']['Volumes']) == 2
+        assert '/var/lib/mysql' in inspect_data['Config']['Volumes']
+        assert '/var/log/mysql' in inspect_data['Config']['Volumes']
 
     def test_entrypoint(self, inspect_data):
-        assert len(inspect_data[0]['Config']['Entrypoint']) == 1
-        assert inspect_data[0]['Config']['Entrypoint'][0] == '/entrypoint.sh'
+        assert len(inspect_data['Config']['Entrypoint']) == 1
+        assert inspect_data['Config']['Entrypoint'][0] == '/entrypoint.sh'
 
     def test_exposed_ports(self, inspect_data):
         if pxc_version_major in ['5.7','5.6']:
-            assert len(inspect_data[0]['Config']['ExposedPorts']) == 3
-            assert '3306/tcp' in inspect_data[0]['Config']['ExposedPorts']
-            assert '4567/tcp' in inspect_data[0]['Config']['ExposedPorts']
-            assert '4568/tcp' in inspect_data[0]['Config']['ExposedPorts']
+            assert len(inspect_data['Config']['ExposedPorts']) == 3
+            assert '3306/tcp' in inspect_data['Config']['ExposedPorts']
+            assert '4567/tcp' in inspect_data['Config']['ExposedPorts']
+            assert '4568/tcp' in inspect_data['Config']['ExposedPorts']
         else:
-            assert len(inspect_data[0]['Config']['ExposedPorts']) == 3
-            assert '3306/tcp' in inspect_data[0]['Config']['ExposedPorts']
-            assert '4567/tcp' in inspect_data[0]['Config']['ExposedPorts']
-            assert '4568/tcp' in inspect_data[0]['Config']['ExposedPorts']
+            assert len(inspect_data['Config']['ExposedPorts']) == 3
+            assert '3306/tcp' in inspect_data['Config']['ExposedPorts']
+            assert '4567/tcp' in inspect_data['Config']['ExposedPorts']
+            assert '4568/tcp' in inspect_data['Config']['ExposedPorts']
