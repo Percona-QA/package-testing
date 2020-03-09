44991cf3be21519a0143c9f9ceaea5b826cdfe68
Tomislav Plavcic <tplavcic@gmail.com>
2020-03-09 10:44:39 +0100

    Revert "Use microdnf instead yum for modifying PXC docker image"
    This reverts commit 0b106493fb8481893c26e765454274fd8d5bed52.

--- docker-image-tests/pxc/tests/test_pxc_static.py
+++ docker-image-tests/pxc/tests/test_pxc_static.py
@@ -12,7 +12,7 @@ container_name = 'pxc-docker-test-static'
 def host():
     docker_id = subprocess.check_output(
         ['docker', 'run', '--name', container_name, '-e', 'MYSQL_ROOT_PASSWORD='+pxc_pwd, '-d', docker_image]).decode().strip()
-    subprocess.check_call(['docker','exec','--user','root',container_name,'microdnf','install','net-tools'])
+    subprocess.check_call(['docker','exec','--user','root',container_name,'yum','install','-y','net-tools'])
     time.sleep(20)
     yield testinfra.get_host("docker://root@" + docker_id)
     subprocess.check_call(['docker', 'rm', '-f', docker_id])
