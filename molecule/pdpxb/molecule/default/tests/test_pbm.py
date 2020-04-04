44627ccca965ae0165aa76b266a33da212c2647a
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-04-04 11:13:36 +0300

    PDMYSQL package tests.

---
+++ molecule/pdpxb/molecule/default/tests/test_pbm.py
@@ -0,0 +1,258 @@
+import os
+import pytest
+import time
+import yaml
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+storage_configs = ['/etc/pbm-agent-storage.conf', '/etc/pbm-agent-storage-gcp.conf',
+                   '/etc/pbm-agent-storage-local.conf']
+
+
+def parse_yaml_string(ys):
+    """Parse yaml string to dictionary
+
+    :param ys:
+    :return:
+    """
+    fd = StringIO(ys)
+    dct = yaml.load(fd)
+    return dct
+
+
+@pytest.fixture()
+def start_stop_pbm(host):
+    """Start and stop pbm-agent service
+
+    :param host:
+    :return:
+    """
+    cmd = "sudo systemctl stop pbm-agent"
+    result = host.run(cmd)
+    assert result.rc == 0, result.stdout
+    cmd = "sudo systemctl start pbm-agent"
+    result = host.run(cmd)
+    assert result.rc == 0, result.stdout
+    cmd = "sudo systemctl status pbm-agent"
+    return host.run(cmd)
+
+
+@pytest.fixture()
+def restart_pbm_agent(host):
+    """Restart pbm-agent service
+    """
+    cmd = "sudo systemctl restart pbm-agent"
+    result = host.run(cmd)
+    assert result.rc == 0, result.stdout
+    cmd = "sudo systemctl status pbm-agent"
+    return host.run(cmd)
+
+
+@pytest.fixture()
+def set_store(host):
+    """Set store for pbm
+
+    :param host:
+    :return:
+    """
+    command = "pbm config --file=/etc/pbm-agent-storage.conf --mongodb-uri=mongodb://localhost:27017/"
+    result = host.run(command)
+    return result
+
+
+@pytest.fixture()
+def show_store(host, set_store):
+    """Show pbm store
+
+    :param host:
+    :param set_store:
+    :return:
+    """
+    command = "pbm config --list --mongodb-uri=mongodb://localhost:27017/?replicaSet=rs1"
+    result = host.run(command)
+    assert result.rc == 0, result.stdout
+    return parse_yaml_string(result.stdout.split("\n", 2)[2].strip())
+
+
+def test_package(host):
+    """Check pbm package
+    """
+    package = host.package("percona-backup-mongodb")
+    assert package.is_installed
+    assert "1.1.1" in package.version, package.version
+
+
+def test_service(host):
+    """Check pbm-agent service
+    """
+    service = host.service("pbm-agent")
+    assert service.is_enabled
+    assert service.is_running
+
+
+def test_pbm_binary(host):
+    """Check pbm binary
+    """
+    file = host.file("/usr/bin/pbm")
+    assert file.user == "root"
+    assert file.group == "root"
+    try:
+        assert file.mode == 0o755
+    except AssertionError:
+        pytest.xfail("Possible xfail")
+
+
+def test_pbm_agent_binary(host):
+    """Check pbm agent binary
+    """
+    file = host.file("/usr/bin/pbm-agent")
+    assert file.user == "root"
+    assert file.group == "root"
+    try:
+        assert file.mode == 0o755
+    except AssertionError:
+        pytest.xfail("Possible xfail")
+
+
+def test_pbm_storage_default_config(host):
+    """Check pbm agent binary
+    """
+    file = host.file("/etc/pbm-storage.conf")
+    assert file.user == "pbm"
+    assert file.group == "pbm"
+    try:
+        assert file.mode == 0o644
+    except AssertionError:
+        pytest.xfail("Possible xfail")
+
+
+# TODO add correct start/stop test
+def test_start_stop_service(start_stop_pbm):
+    """Start and stop pbm agent
+
+    :param start_stop_pbm:
+    """
+    assert start_stop_pbm.rc == 0, restart_pbm_agent.stdout
+    assert "active" in start_stop_pbm.stdout, restart_pbm_agent.stdout
+
+
+def test_restart_service(restart_pbm_agent):
+    """Restart pbm agent
+
+    :param restart_pbm_agent:
+    """
+    assert restart_pbm_agent.rc == 0, restart_pbm_agent.stdout
+    assert "active" in restart_pbm_agent.stdout
+
+
+def test_pbm_version(host):
+    """Check that pbm version is not empty strings
+
+    :param host:
+    :return:
+    """
+    result = host.run("pbm version")
+    assert result.rc == 0, result.stdout
+    print(result.stdout)
+    lines = result.stdout.split("\n")
+    parsed_config = {line.split(":")[0]: line.split(":")[1].strip() for line in lines[0:-1]}
+    assert parsed_config['Version'] == '1.1.1', parsed_config
+    assert parsed_config['Platform'], parsed_config
+    assert parsed_config['GitCommit'], parsed_config
+    assert parsed_config['GitBranch'], parsed_config
+    assert parsed_config['BuildTime'], parsed_config
+    assert parsed_config['GoVersion'], parsed_config
+
+
+def test_pbm_help(host):
+    """Check that pbm have help message
+
+    :param host:
+    :return:
+    """
+    result = host.run("pbm help")
+    assert result.rc == 0, result.stdout
+
+
+def test_set_store(set_store):
+    """Set and show storage test
+
+    :param set_store:
+    :return:
+    """
+    assert set_store.rc == 0, set_store.stdout
+    store_out = parse_yaml_string("\n".join(set_store.stdout.split("\n")[2:-2]))
+    assert store_out['storage']['type'] == 's3'
+    assert store_out['storage']['s3']['region'] == 'us-east-1'
+    assert store_out['storage']['s3']['bucket'] == 'operator-testing'
+
+
+def test_show_store(show_store):
+    """Check that all store configuration paramateres presented
+
+    :param show_store:
+    :return:
+    """
+    assert show_store['s3']
+    assert show_store['s3']['region'] == 'us-east-1'
+    assert show_store['s3']['bucket'] == 'operator-testing'
+
+
+@pytest.mark.parametrize("store", storage_configs, ids=['aws', 'gcp', 'local'])
+def test_backup_and_restore(host, store):
+    """ Insert some data, make backup, check that backup in list, drop data, restore.
+    """
+    drop_data = """mongo --quiet --eval 'db.dropDatabase()' test"""
+    host.run(drop_data)
+    command = "pbm config --file={} --mongodb-uri=mongodb://localhost:27017/".format(store)
+    result = host.run(command)
+    assert result.rc == 0, result.stderr
+    time.sleep(180)
+    print(host.run('pbm config --list --mongodb-uri=mongodb://localhost:27017/'))
+    insert_data = """mongo --quiet --eval 'for(
+        i=1; i <= 100000; i++) { db.test.insert( {_id: i, name: "Test_"+i })}' test"""
+    insert_data_result = host.run(insert_data)
+    assert insert_data_result.rc == 0, insert_data_result.stdout
+    assert insert_data_result.stdout.strip("\n") == """WriteResult({ "nInserted" : 1 })""", insert_data_result.stdout
+    save_hash = """mongo --quiet --eval 'db.runCommand({ dbHash: 1 }).md5' test|tail -n1"""
+    save_hash_result = host.run(save_hash)
+    assert save_hash_result.rc == 0, save_hash_result.stdout
+    hash_before = save_hash_result.stdout.strip("\n")
+    backup = """pbm backup --mongodb-uri=mongodb://localhost:27017"""
+    backup_result = host.run(backup)
+    print(backup_result.stdout)
+    print(backup_result.stderr)
+    assert 'Starting' in backup_result.stdout, backup_result.stdout
+    time.sleep(180)
+    backup_name = backup_result.stdout.split()[2].strip("\'").rstrip("'...")
+    cmd = "pbm list --mongodb-uri=mongodb://localhost:27017"
+    result = host.run(cmd)
+    assert result.rc == 0, result.stdout
+    assert backup_name in result.stdout, result.stdout
+    drop_data = """mongo --quiet --eval 'db.dropDatabase()' test"""
+    drop_data_result = host.run(drop_data)
+    assert drop_data_result.rc == 0, drop_data_result.stdout
+    documents_after_drop = """mongo --quiet --eval 'db.test.count()' test|tail -n1"""
+    result = host.run(documents_after_drop)
+    assert result.rc == 0, result.stdout
+    assert result.stdout.split("\n")[0] == "0"
+    restore = """pbm restore --mongodb-uri=mongodb://localhost:27017 {}""".format(backup_name)
+    restore_result = host.run(restore)
+    print(restore_result.stdout)
+    print(restore_result.stderr)
+    # assert restore_result.rc == 0, restore_result.stdout
+    time.sleep(360)
+    db_hash_after = """mongo --quiet --eval 'db.runCommand({ dbHash: 1 }).md5' test|tail -n1"""
+    db_hash_after_result = host.run(db_hash_after)
+    db_hash_after_value = db_hash_after_result.stdout.strip("\n")
+    assert db_hash_after_result.rc == 0, db_hash_after_result.stdout
+    print(hash_before)
+    print(db_hash_after_value)
+    assert hash_before == db_hash_after_value
---
+++ molecule/pdpxb/molecule/tests/test_tools_extended.py
@@ -0,0 +1,177 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+from .settings import versions
+
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+# PACKAGES = ["libecpg-compat3", "libecpg-compat3-dbgsym", "libecpg-dev-dbgsym", "libecpg-dev", "libecpg6-dbgsym",
+#             'libecpg6', "libpgtypes3", "libpgtypes3-dbgsym", "libpq-dev", "libpq5-dbgsym", "libpq5"]
+
+PACKAGES = ["libecpg-compat3",  "libecpg-dev", 'libecpg6', "libpgtypes3", "libpq-dev",  "libpq5"]
+pg_versions = versions[os.getenv("PG_VERSION")]
+
+# @pytest.fixture()
+# def fdw_extension(host):
+#     with host.sudo("postgres"):
+#         install_extension = host.run("psql -c 'CREATE EXTENSION \"postgres_fdw\";'")
+#         try:
+#             assert install_extension.rc == 0
+#             assert install_extension.stdout.strip("\n") == "CREATE EXTENSION"
+#         except AssertionError:
+#             pytest.fail("Return code {}. Stderror: {}. Stdout {}".format(install_extension.rc,
+#                                                                          install_extension.stderr,
+#                                                                          install_extension.stdout))
+#
+#         try:
+#             extensions = host.run("psql -c 'SELECT * FROM pg_extension;' | awk 'NR>=3{print $1}'")
+#             assert extensions.rc == 0
+#             assert "postgres_fdw" in set(extensions.stdout.split())
+#         except AssertionError:
+#             pytest.fail("Return code {}. Stderror: {}. Stdout {}").format(extensions.rc, extensions.stderr,
+#                                                                           extensions.stdout)
+#
+#
+# @pytest.fixture()
+# def fdw_functional(host):
+#     pass
+
+
+@pytest.fixture()
+def pythonu_function(host):
+    ds = host.system_info.distribution
+    if ds.lower() in ['debian', 'ubuntu'] and os.getenv("PG_VERSION") != 'ppg-11.5':
+        pytest.skip("Skipping python2 extensions for DEB based in 11.6 pg")
+    with host.sudo("postgres"):
+        install_extension = host.run("psql -c 'CREATE EXTENSION IF NOT EXISTS\"plpythonu\";'")
+        assert install_extension.rc == 0
+        create_function = """CREATE FUNCTION pymax (a integer, b integer)
+          RETURNS integer
+        AS $$
+          if a > b:
+            return a
+          return b
+        $$ LANGUAGE plpythonu;
+                """
+        execute_psql = host.run("psql -c \'{}\'".format(create_function))
+        assert execute_psql.rc == 0
+        assert execute_psql.stdout.strip("\n") == "CREATE FUNCTION"
+        return execute_psql
+
+
+@pytest.fixture()
+def perl_function(host):
+    with host.sudo("postgres"):
+        install_extension = host.run("psql -c 'CREATE EXTENSION IF NOT EXISTS \"plperl\";'")
+        assert install_extension.rc == 0
+        create_function = """CREATE FUNCTION perl_max (integer, integer) RETURNS integer AS $$
+    if ($_[0] > $_[1]) { return $_[0]; }
+    return $_[1];
+$$ LANGUAGE plperl;
+        """
+        execute_psql = host.run("psql -c \'{}\'".format(create_function))
+        assert execute_psql.rc == 0
+        assert execute_psql.stdout.strip("\n") == "CREATE FUNCTION"
+        return execute_psql
+
+
+@pytest.fixture()
+def python3_function(host):
+    os = host.system_info.distribution
+    if os.lower() in ['centos', 'redhat', 'rhel']:
+        pytest.skip("Skipping python3 extensions for Centos or RHEL")
+    with host.sudo("postgres"):
+        install_extension = host.run("psql -c 'CREATE EXTENSION IF NOT EXISTS \"plpython3u\";'")
+        assert install_extension.rc == 0
+        create_function = """CREATE FUNCTION pymax3 (a integer, b integer)
+                  RETURNS integer
+                AS $$
+                  if a > b:
+                    return a
+                  return b
+                $$ LANGUAGE plpython3u;
+                        """
+        execute_psql = host.run("psql -c \'{}\'".format(create_function))
+        assert execute_psql.rc == 0, execute_psql.stderr
+        assert execute_psql.stdout.strip("\n") == "CREATE FUNCTION", execute_psql.stdout
+        return execute_psql
+
+
+@pytest.fixture()
+def tcl_function(host):
+    with host.sudo("postgres"):
+        install_extension = host.run("psql -c 'CREATE EXTENSION IF NOT EXISTS \"pltcl\";'")
+        assert install_extension.rc == 0
+        create_function = """CREATE FUNCTION tcl_max(integer, integer) RETURNS integer AS $$
+    if {$1 > $2} {return $1}
+    return $2
+$$ LANGUAGE pltcl STRICT;
+        """
+        execute_psql = host.run("psql -c \'{}\'".format(create_function))
+        assert execute_psql.rc == 0
+        assert execute_psql.stdout.strip("\n") == "CREATE FUNCTION"
+        return execute_psql
+
+
+@pytest.fixture()
+def build_libpq_programm(host):
+    os = host.system_info.distribution
+    pg_include_cmd = "pg_config --includedir"
+    pg_include = host.check_output(pg_include_cmd)
+    lib_dir_cmd = "pg_config --libdir"
+    lib_dir = host.check_output(lib_dir_cmd)
+    if os in ["centos", 'rhel', "redhat"]:
+        return host.run(
+            "export LIBPQ_DIR=/usr/pgsql-11/  && export LIBRARY_PATH=/usr/pgsql-11/lib/ &&"
+            "gcc -o lib_version /tmp/libpq_command_temp_dir/lib_version.c -I{} -lpq -std=c99".format(pg_include))
+    return host.run(
+        "gcc -o lib_version /tmp/libpq_command_temp_dir/lib_version.c -I{} -lpq -std=c99".format(pg_include))
+
+
+@pytest.mark.parametrize("package", PACKAGES)
+def test_deb_package_is_installed(host, package):
+    os = host.system_info.distribution
+    if os.lower() in ["redhat", "centos", 'rhel']:
+        pytest.skip("This test only for Debian based platforms")
+    pkg = host.package(package)
+    assert pkg.is_installed
+    assert pkg.version in pg_versions['deb_pkg_ver']
+
+
+def test_build_libpq_programm(host, build_libpq_programm):
+    assert build_libpq_programm.rc == 0
+    libpq_version = host.run("./lib_version ")
+    assert libpq_version.stdout.strip("\n") == pg_versions['libpq']
+    assert libpq_version.rc == 0
+
+
+def test_pythonu_function(host, pythonu_function):
+    with host.sudo("postgres"):
+        result = host.run("psql -c \'SELECT pymax(1, 2);\' | awk 'NR>=3{print $1}'")
+        assert result.rc == 0
+        assert result.stdout.strip("\n(1") == "2"
+
+
+def test_perl_function(host, perl_function):
+    with host.sudo("postgres"):
+        result = host.run("psql -c \'SELECT perl_max(1, 2);\' | awk 'NR>=3{print $1}'")
+        assert result.rc == 0
+        assert result.stdout.strip("\n(1") == "2"
+
+
+def test_tcl_function(host, tcl_function):
+    with host.sudo("postgres"):
+        result = host.run("psql -c \'SELECT tcl_max(1, 2);\' | awk 'NR>=3{print $1}'")
+        assert result.rc == 0
+        assert result.stdout.strip("\n(1") == "2"
+
+
+def test_python3(host, python3_function):
+    with host.sudo("postgres"):
+        result = host.run("psql -c \'SELECT pymax3(1, 2);\' | awk 'NR>=3{print $1}'")
+        assert result.rc == 0
+        assert result.stdout.strip("\n(1") == "2"
