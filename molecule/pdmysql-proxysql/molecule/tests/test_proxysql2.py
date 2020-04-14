6a468975855aef66a396cbbf3ccda0f8fb16f815
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-04-14 14:42:34 +0300

    PDMYSQL. Fix for proxysql.

--- molecule/pdmysql-proxysql/molecule/tests/test_proxysql2.py
+++ molecule/pdmysql-proxysql/molecule/tests/test_proxysql2.py
@@ -8,11 +8,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_proxysql2_version(host):
     cmd = 'proxysql --version'
-    if os.lower() in ["redhat", "centos", 'rhel']:
-        cmd_pkg = host.run("repoquery -i proxysql2")
-    else:
-        cmd_pkg = host.run("apt-cache showpkg proxysql2")
-    print(cmd_pkg.stdout, cmd_pkg.stderr)
     result = host.run(cmd)
     assert result.rc == 0, result.stderr
     assert '2.0.10' in result.stdout, result.stdout
