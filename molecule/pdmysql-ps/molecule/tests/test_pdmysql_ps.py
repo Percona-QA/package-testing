ff0e9bc8d45dfcb35ac5047d8212bb0766de5b71
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-04-14 10:32:38 +0300

    PDMYSQL. PS plugins test from bash to python.

--- molecule/pdmysql-ps/molecule/tests/test_pdmysql_ps.py
+++ molecule/pdmysql-ps/molecule/tests/test_pdmysql_ps.py
@@ -18,6 +18,53 @@ RPMPACKAGES = ['percona-server-server', 'percona-server-client',
                'percona-server-rocksdb', 'percona-mysql-router',
                'percona-mysql-shell']
 
+PLUGIN_COMMANDS = ["mysql -e \"CREATE FUNCTION"
+                   " fnv1a_64 RETURNS INTEGER SONAME 'libfnv1a_udf.so'\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so'\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " murmur_hash RETURNS INTEGER SONAME 'libmurmur_udf.so'\"",
+                   "mysql -e \"INSTALL PLUGIN"
+                   " audit_log SONAME 'audit_log.so';\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " version_tokens_set RETURNS STRING SONAME 'version_token.so';\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " version_tokens_show RETURNS STRING SONAME 'version_token.so';\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " version_tokens_edit RETURNS STRING SONAME 'version_token.so';\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " version_tokens_delete RETURNS STRING SONAME 'version_token.so';\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " version_tokens_lock_shared RETURNS INT SONAME 'version_token.so';\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " version_tokens_lock_exclusive RETURNS INT SONAME 'version_token.so';\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " version_tokens_unlock RETURNS INT SONAME 'version_token.so';\"",
+                   "mysql -e \"INSTALL PLUGIN"
+                   " mysql_no_login SONAME 'mysql_no_login.so';\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " service_get_read_locks RETURNS INT SONAME 'locking_service.so';\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " service_get_write_locks RETURNS INT SONAME 'locking_service.so';\"",
+                   "mysql -e \"CREATE FUNCTION"
+                   " service_release_locks RETURNS INT SONAME 'locking_service.so';\"",
+                   "mysql -e \"INSTALL PLUGIN"
+                   " validate_password SONAME 'validate_password.so';\"",
+                   "mysql -e \"INSTALL PLUGIN"
+                   " version_tokens SONAME 'version_token.so';\"",
+                   "mysql -e \"INSTALL PLUGIN"
+                   " rpl_semi_sync_master SONAME 'semisync_master.so';\"",
+                   "mysql -e \"INSTALL PLUGIN"
+                   " rpl_semi_sync_slave SONAME 'semisync_slave.so';\"",
+                   "mysql -e \"INSTALL PLUGIN"
+                   " connection_control SONAME 'connection_control.so';\"",
+                   "mysql -e \"INSTALL PLUGIN"
+                   " connection_control_failed_login_attempts SONAME 'connection_control.so';\""]
+
+COMPONENTS = ['component_validate_password', 'component_log_sink_syseventlog',
+              'component_log_sink_json', 'component_log_filter_dragnet',
+              'component_audit_api_message_emit']
+
 
 @pytest.mark.parametrize("package", DEBPACKAGES)
 def test_check_deb_package(host, package):
@@ -58,3 +105,24 @@ def test_mysql_version(host, component):
     result = host.run(cmd)
     print(result.stdout)
     assert result.rc == 0, result.stderr
+
+
+@pytest.mark.parametrize('plugin_command', PLUGIN_COMMANDS)
+def test_plugins(host, plugin_command):
+    result = host.run(plugin_command)
+    print(result.stdout)
+    assert result.rc == 0, result.stderr
+
+
+@pytest.mark.parametrize("component", COMPONENTS)
+def test_components(component, host):
+    cmd = 'mysql -Ns -e "select count(*) from mysql.component where component_urn=\"file://{}\";"'.format(component)
+    check_component = host.run(cmd)
+    if check_component.rc == 0:
+        inst_cmd = 'mysql -e "INSTALL COMPONENT \"file://{}\";"'.format(component)
+        inst_res = host.run(inst_cmd)
+        assert inst_res.rc == 0, inst_res.stderr
+    check_cmd = 'mysql -Ns -e "select count(*) from mysql.component where component_urn=\"file://{}\";"'.format(
+        component)
+    check_result = host.run(check_cmd)
+    assert check_result.rc == 1, (check_result.rc, check_result.stderr, check_result.stdout)
