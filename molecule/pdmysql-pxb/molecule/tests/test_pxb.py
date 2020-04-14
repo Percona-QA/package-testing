369bc36b0cc2e48c24c464f8dc1d17362ae2bcf6
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-04-14 11:12:27 +0300

    PDMYSQL. Change pxb version.

--- molecule/pdmysql-pxb/molecule/tests/test_pxb.py
+++ molecule/pdmysql-pxb/molecule/tests/test_pxb.py
@@ -24,7 +24,7 @@ def test_check_deb_package(host, package):
     print(cmd.stdout)
     pkg = host.package(package)
     assert pkg.is_installed
-    assert '8.0.18' in pkg.version, pkg.version
+    assert '8.0.11' in pkg.version, pkg.version
 
 
 @pytest.mark.parametrize("package", RPMPACKAGES)
@@ -36,26 +36,28 @@ def test_check_rpm_package(host, package):
     print(cmd.stdout)
     pkg = host.package(package)
     assert pkg.is_installed
-    assert '8.0.18' in pkg.version, pkg.version
+    assert '8.0.11' in pkg.version, pkg.version
 
 
 def test_binary_version(host):
     cmd = "xtrabackup --version"
     result = host.run(cmd)
-    print(result.stdout)
+    print(result.stderr)
     assert result.rc == 0, result.stderr
-    assert '8.0.18' in result.stdout, result.stdout
+    assert '8.0.11' in result.stderr, (result.stdout, result.stdout)
 
 
 def test_run_backup(host):
-    cmd = "/usr/bin/xtrabackup --backup --user=root --target-dir=/tmp/backups/"
-    result = host.run(cmd)
-    print(result.stdout)
-    assert result.rc == 0, result.stderr
+    with host.sudo("root"):
+        cmd = "/usr/bin/xtrabackup --backup --user=root --target-dir=/tmp/backups/"
+        result = host.run(cmd)
+        print(result.stdout)
+        assert result.rc == 0, result.stderr
 
 
 def test_run_prepare(host):
-    cmd = "/usr/bin/xtrabackup --prepare --user=root --target-dir=/tmp/backups/"
-    result = host.run(cmd)
-    print(result.stdout)
-    assert result.rc == 0, result.stderr
+    with host.sudo("root"):
+        cmd = "/usr/bin/xtrabackup --prepare --user=root --target-dir=/tmp/backups/"
+        result = host.run(cmd)
+        print(result.stdout)
+        assert result.rc == 0, result.stderr
