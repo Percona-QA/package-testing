1f0858493f244b391b223505c56a1f5a9dcbdb3a
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-04-23 17:26:31 +0300

    PDMYSQL. Rewrite pxb pt bash tests to python.

--- molecule/pdmysql-pxb/molecule/tests/test_pxb.py
+++ molecule/pdmysql-pxb/molecule/tests/test_pxb.py
@@ -14,6 +14,15 @@ RPMPACKAGES = ['percona-xtrabackup-80',
                'percona-xtrabackup-test-80',
                'percona-xtrabackup-80-debuginfo']
 
+PTBINS = ['pt-align', 'pt-archiver', 'pt-config-diff', 'pt-deadlock-logger', 'pt-diskstats',
+          'pt-duplicate-key-checker', 'pt-fifo-split', 'pt-find', 'pt-fingerprint',
+          'pt-fk-error-logger', 'pt-heartbeat', 'pt-index-usage', 'pt-ioprofile', 'pt-kill',
+          'pt-mext', 'pt-mongodb-query-digest', 'pt-mongodb-summary', 'pt-mysql-summary',
+          'pt-online-schema-change', 'pt-pmp', 'pt-query-digest', 'pt-show-grants', 'pt-sift',
+          'pt-slave-delay', 'pt-slave-find', 'pt-slave-restart', 'pt-stalk' 'pt-summary',
+          'pt-table-checksum', 'pt-table-sync', 'pt-table-usage', 'pt-upgrade',
+          'pt-variable-advisor', 'pt-visual-explain']
+
 
 @pytest.mark.parametrize("package", DEBPACKAGES)
 def test_check_deb_package(host, package):
@@ -40,6 +49,14 @@ def test_binary_version(host):
     result = host.run(cmd)
     assert result.rc == 0, result.stderr
     assert '8.0.11' in result.stderr, (result.stdout, result.stdout)
+
+
+@pytest.mark.parametrize("pt_bin", PTBINS)
+def test_pt_binaries(host, pt_bin):
+    cmd = '{} --help'.format(pt_bin)
+    result = host.run(cmd)
+    print(result.stdout)
+    assert result.rc == 0, result.stderr
 
 
 def test_run_backup(host):
