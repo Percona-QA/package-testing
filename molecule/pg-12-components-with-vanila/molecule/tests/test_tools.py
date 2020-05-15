a1143ec1b397ab862441095c3459975f4a725472
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-05-15 08:54:47 +0300

    PG12. Rename pgaudit

--- molecule/pg-12-components-with-vanila/molecule/tests/test_tools.py
+++ molecule/pg-12-components-with-vanila/molecule/tests/test_tools.py
@@ -235,7 +235,8 @@ def test_pgaudit_package(host):
     os = host.system_info.distribution
     pkgn = ""
     if os.lower() in ["redhat", "centos", 'rhel']:
-        pkgn = "percona-pgaudit"
+        # pkgn = "percona-pgaudit"
+        pkgn = "percona-pgaudit14_12"
     elif os in ["debian", "ubuntu"]:
         pkgn = "percona-postgresql-12-pgaudit"
         dbgsym_pkgn = "percona-postgresql-12-pgaudit-dbgsym"
