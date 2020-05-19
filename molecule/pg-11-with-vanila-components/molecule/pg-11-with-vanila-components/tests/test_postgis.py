1eef834d4051c5701848d8c8734b10f6c89d7723
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-05-19 19:40:05 +0300

    PPG11. Check psql version for postgis.

--- molecule/pg-11-with-vanila-components/molecule/pg-11-with-vanila-components/tests/test_postgis.py
+++ molecule/pg-11-with-vanila-components/molecule/pg-11-with-vanila-components/tests/test_postgis.py
@@ -12,3 +12,15 @@ def test_create_postigs_extension(host):
         install_extension = host.run("psql -c 'CREATE EXTENSION \"postgis\";'")
         assert install_extension.rc == 0, install_extension.stderr
         assert install_extension.stdout.strip("\n") == "CREATE EXTENSION", install_extension.stderr
+
+
+def test_show_postgresql_version(host):
+    rel = host.system_info.distribution
+    cmd = ""
+    if rel.lower() in ["debian", "ubuntu"]:
+        cmd = "apt list *postgresql*"
+    elif rel.lower() in ["redhat", "centos", 'rhel']:
+        cmd = "yum list --show-duplicates *postgresql*"
+    with host.sudo():
+        result = host.run(cmd)
+        print(result.stdout)
