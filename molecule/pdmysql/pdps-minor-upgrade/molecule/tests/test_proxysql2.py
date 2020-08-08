00f9fb2183f4580064e5991027240c51d19e16df
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-08-08 16:46:39 +0300

    PDPS. Fix minor upgrade issue test.

---
+++ molecule/pdmysql/pdps-minor-upgrade/molecule/tests/test_proxysql2.py
@@ -0,0 +1,18 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_package_is_installed(host):
+    pkg = host.package('proxysql2')
+    assert pkg.is_installed
+
+
+def test_proxysql2_version(host):
+    cmd = 'proxysql --version'
+    result = host.run(cmd)
+    assert result.rc == 0, result.stderr
+    assert '2.0.12' in result.stdout, result.stdout
