119864026908bd5e1e4420a0a84f7bcbdcbb64ef
sandraromanchenko <sandra.romanchenko@percona.com>
2020-08-20 14:16:18 +0300

    PBM. Add proper permissions for config file and set correct version

--- molecule/pbm/molecule/tests/test_pbm.py
+++ molecule/pbm/molecule/tests/test_pbm.py
@@ -194,7 +194,7 @@ def test_pbm_version(host):
     assert result.rc == 0, result.stdout
     lines = result.stdout.split("\n")
     parsed_config = {line.split(":")[0]: line.split(":")[1].strip() for line in lines[0:-1]}
-    assert parsed_config['Version'] == '1.3.0', parsed_config
+    assert parsed_config['Version'] == 'v1.3.0', parsed_config
     assert parsed_config['Platform'], parsed_config
     assert parsed_config['GitCommit'], parsed_config
     assert parsed_config['GitBranch'], parsed_config
