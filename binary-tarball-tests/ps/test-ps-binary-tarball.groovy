633bfbe1040cf4e8243f86a1b8bc92ea3cd0f3b8
hrvojem <hmatijak@gmail.com>
2020-08-24 14:47:45 +0200

    Add support for minimal tarballs

--- binary-tarball-tests/ps/test-ps-binary-tarball.groovy
+++ binary-tarball-tests/ps/test-ps-binary-tarball.groovy
@@ -3,6 +3,10 @@ pipeline {
   parameters {
     string(name: 'PS_VERSION', defaultValue: '8.0.20-11', description: 'PS full version')
     string(name: 'PS_REVISION', defaultValue: '159f0eb', description: 'PS revision')
+    booleanParam( 
+      defaultValue: false,
+      name: 'BUILD_TYPE_MINIMAL'
+    )
   }
   stages {
     stage('Binary tarball test') {
@@ -47,11 +51,15 @@ pipeline {
             withCredentials([usernamePassword(credentialsId: 'JenkinsAPI', passwordVariable: 'JENKINS_API_PWD', usernameVariable: 'JENKINS_API_USER')]) {
               sh '''
                 PS_MAJOR_VERSION="$(echo ${PS_VERSION}|cut -d'.' -f1,2)"
+                MINIMAL=""
+                if [ BUILD_TYPE_MINIMAL ]; then
+                  MINIMAL="-minimal"
+                fi
                 if [ "${PS_MAJOR_VERSION}" = "8.0" ]; then
                   TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.12.tar.gz" 
                   JENKINS_JOB="https://jenkins.percona.com/job/percona-server-${PS_MAJOR_VERSION}-binaries-release/label_exp=min-centos-6-x64/lastSuccessfulBuild/artifact/tarball/"
                 elif [ "${PS_MAJOR_VERSION}" = "5.7" ]; then
-                  TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.12.tar.gz"
+                  TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.12{MINIMAL}.tar.gz"
                   JENKINS_JOB="https://jenkins.percona.com/job/percona-server-${PS_MAJOR_VERSION}-binaries-release-rocks-new/lastSuccessfulBuild/label_exp=min-centos-6-x64-new/artifact/tarball/"
                 elif [ "${PS_MAJOR_VERSION}" = "5.6" ]; then
                   TARBALL_NAME="Percona-Server-$(echo ${PS_VERSION}|sed 's/-/-rel/')-Linux.x86_64.ssl100.tar.gz"
