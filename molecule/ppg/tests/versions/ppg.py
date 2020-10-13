332663bcc67557dfcccd1c57df477b063a96d419
Mikhail Samoylov <mikhail.samoylov@percona.com>
2020-10-13 17:06:59 +0300

    PPG13. Update versions.

--- molecule/ppg/tests/versions/ppg.py
+++ molecule/ppg/tests/versions/ppg.py
@@ -267,6 +267,6 @@ def get_ppg_versions(distro_type):
                                           distros=DISTROS, distro_type=distro_type),
             "ppg-12.4": get_pg12_versions(packages=["2:12-4.2", "12+216-3", '216-3'],
                                           distros=DISTROS, distro_type=distro_type),
-            "ppg-13.0": get_pg13_versions(packages=["2:13-0.2", "13+216-3", '216-3'],
+            "ppg-13.0": get_pg13_versions(packages=["2:13-0.1", "13+219-1", '219-1'],
                                           distros=DISTROS, distro_type=distro_type)
             }
