9a2aeb8b0d8aff68279e476e8274a623e3c23061
hrvojem <hmatijak@gmail.com>
2020-12-15 18:25:59 +0100

    Version update

--- binary-tarball-tests/pxc/settings.py
+++ binary-tarball-tests/pxc/settings.py
@@ -52,7 +52,7 @@ pxc80_symlinks = (
   ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.21.1.21'), ('lib/libplc4.so','lib/private/libplc4.so'),
   ('lib/libplds4.so','lib/private/libplds4.so'), ('lib/libsasl2.so','lib/private/libsasl2.so.3.0.0'),
   ('lib/libsmime3.so','lib/private/libsmime3.so'), ('lib/libssl.so','lib/private/libssl.so.1.0.2k'),
-  ('lib/libssl3.so','lib/private/libssl3.so'), ('lib/libtinfo.so','lib/private/libtinfo.so.5.7'),
+  ('lib/libssl3.so','lib/private/libssl3.so'), ('lib/libtinfo.so','lib/private/libtinfo.so.5.9'),
 )
 
 # 5.7
