cbc3b9470f2d568391fd7dfa80d09788170bb9ea
hrvojem <hmatijak@gmail.com>
2021-03-16 17:49:29 +0100

    Version updated

--- binary-tarball-tests/pxc/test-pxc-binary-tarball.groovy
+++ binary-tarball-tests/pxc/test-pxc-binary-tarball.groovy
@@ -4,7 +4,7 @@ pipeline {
     string(name: 'PXC_VERSION', defaultValue: '8.0.22-13.1', description: 'PXC full version')
     string(name: 'PXC_REVISION', defaultValue: '428f061', description: 'PXC revision')
     string(name: 'WSREP_VERSION', defaultValue: '26.4.3', description: 'WSREP version')
-    string(name: 'PXC57_PKG_VERSION', defaultValue: '5.7.31-rel34-43.2', description: 'PXC-5.7 package version')
+    string(name: 'PXC57_PKG_VERSION', defaultValue: '5.7.33-rel36-49.1', description: 'PXC-5.7 package version')
     booleanParam( 
       defaultValue: false,
       name: 'BUILD_TYPE_MINIMAL'
