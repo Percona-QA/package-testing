e0123428ad3e5d854dc88008115ec33d2de5de3a
hrvojem <hmatijak@gmail.com>
2021-05-11 22:47:57 +0200

    CentOS6 fix

--- binary-tarball-tests/ps/test-ps-binary-tarball.groovy
+++ binary-tarball-tests/ps/test-ps-binary-tarball.groovy
@@ -120,7 +120,31 @@ pipeline {
                 currentBuild.displayName = "#${BUILD_NUMBER}-${PS_VERSION}-${PS_REVISION}"
               }
             withCredentials([usernamePassword(credentialsId: 'JenkinsAPI', passwordVariable: 'JENKINS_API_PWD', usernameVariable: 'JENKINS_API_USER')]) {
-              run_test()
+	      sh '''
+                echo ${BUILD_TYPE_MINIMAL}
+                PS_MAJOR_VERSION="$(echo ${PS_VERSION}|cut -d'.' -f1,2)"
+                MINIMAL=""
+                if [ "${BUILD_TYPE_MINIMAL}" = "true" ]; then
+                  MINIMAL="-minimal"
+                fi
+                if [ "${PS_MAJOR_VERSION}" = "8.0" ]; then
+                  TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.12${MINIMAL}.tar.gz"
+                  TARBALL_LINK="https://www.percona.com/downloads/TESTING/ps-${PS_VERSION}/"
+                elif [ "${PS_MAJOR_VERSION}" = "5.7" ]; then
+                  TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc2.12${MINIMAL}.tar.gz"
+                  TARBALL_LINK="https://www.percona.com/downloads/TESTING/ps-${PS_VERSION}/"
+                fi
+                rm -rf package-testing
+                if [ -f /usr/bin/yum ]; then
+                  sudo yum install -y git wget
+                else
+                  sudo apt install -y git wget lsb-release
+                fi
+                git clone https://github.com/Percona-QA/package-testing.git --branch master --depth 1
+                cd package-testing/binary-tarball-tests/ps
+                wget -q ${TARBALL_LINK}${TARBALL_NAME}
+                ./run.sh || true
+	      '''
             }
             junit 'package-testing/binary-tarball-tests/ps/report.xml'
           } //End steps
