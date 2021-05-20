d941f28b8c6890e62a171c1ac153f5160459f1d8
Noemi Lapresta <noemi.lapresta@percona.com>
2021-05-20 13:56:27 +0200

    Add Centos6 support for PXC 5.7 tarball tests

--- binary-tarball-tests/pxc/test-pxc-binary-tarball.groovy
+++ binary-tarball-tests/pxc/test-pxc-binary-tarball.groovy
@@ -71,6 +71,22 @@ pipeline {
             junit 'package-testing/binary-tarball-tests/pxc/report.xml'
           } //End steps
         } //End stage Debian Buster
+        stage('Centos6') {
+          when {
+            expression {
+              params.PXC_VERSION[0..2] == "5.7"
+            }
+          }
+          agent {
+            label "min-centos-6-x64"
+          }
+          steps {
+            withCredentials([usernamePassword(credentialsId: 'JenkinsAPI', passwordVariable: 'JENKINS_API_PWD', usernameVariable: 'JENKINS_API_USER')]) {
+              run_test()
+            }
+            junit 'package-testing/binary-tarball-tests/pxc/report.xml'
+          } //End steps
+        } //End stage CentOS6
         stage('Centos7') {
           agent {
             label "min-centos-7-x64"
@@ -110,7 +126,11 @@ void run_test() {
       TARBALL_NAME="Percona-XtraDB-Cluster_${PXC_VERSION}_Linux.x86_64.glibc2.17${MINIMAL}.tar.gz"
       TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/pxc-${PXC_VERSION}/"
     elif [ "${PXC_MAJOR_VERSION}" = "5.7" ]; then
-      TARBALL_NAME="Percona-XtraDB-Cluster-${PXC57_PKG_VERSION}.Linux.x86_64.glibc2.17${MINIMAL}.tar.gz"
+      export GLIBC_VERSION="2.17"
+      if [ -f /etc/redhat-release ] && [ $(grep -c "release 6" /etc/redhat-release) -eq 1 ]; then
+        export GLIBC_VERSION="2.12"
+      fi
+      TARBALL_NAME="Percona-XtraDB-Cluster-${PXC57_PKG_VERSION}.Linux.x86_64.glibc${GLIBC_VERSION}${MINIMAL}.tar.gz"
       TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/pxc-${PXC_VERSION}/"
     fi
     rm -rf package-testing
