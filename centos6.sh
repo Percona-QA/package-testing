c42b598b89e3fb61e17c30517f755cad112806ed
Noemi Lapresta <noemi.lapresta@percona.com>
2021-05-21 12:52:35 +0200

    Use local files in centos6.sh script

--- centos6.sh
+++ centos6.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-curl https://www.getpagespeed.com/files/centos6-eol.repo --output /etc/yum.repos.d/CentOS-Base.repo
-curl https://www.getpagespeed.com/files/centos6-epel-eol.repo --output /etc/yum.repos.d/epel.repo
-cp -pvr /vagrant/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo
+
+cp $(dirname $0)/centos6-repos/centos6-eol.repo /etc/yum.repos.d/CentOS-Base.repo
+cp $(dirname $0)/centos6-repos/centos6-epel-eol.repo /etc/yum.repos.d/epel.repo
 yum -y install centos-release-scl
-curl https://www.getpagespeed.com/files/centos6-scl-eol.repo --output /etc/yum.repos.d/CentOS-SCLo-scl.repo
-curl https://www.getpagespeed.com/files/centos6-scl-rh-eol.repo --output /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
+cp $(dirname $0)/centos6-repos/centos6-scl-eol.repo /etc/yum.repos.d/CentOS-SCLo-scl.repo
+cp $(dirname $0)/centos6-repos/centos6-scl-rh-eol.repo /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
 yum -y install epel-release.noarch
