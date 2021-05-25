853abfb25cba64f9b3d1e1dd40f8bdd1c9089476
Noemi Lapresta <noemi.lapresta@percona.com>
2021-05-25 15:13:01 +0200

    Check symlink destination exists

--- binary-tarball-tests/ps/tests/test_ps_static.py
+++ binary-tarball-tests/ps/tests/test_ps_static.py
@@ -26,3 +26,4 @@ def test_symlinks(host):
     for symlink in ps_symlinks:
         assert host.file(base_dir+'/'+symlink[0]).is_symlink
         assert host.file(base_dir+'/'+symlink[0]).linked_to == base_dir+'/'+symlink[1]
+        assert host.file(base_dir+'/'+symlink[1]).exists
