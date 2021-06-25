b5247e31a7ae57662761f7c7b1332be5d40d7319
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-06-25 22:52:33 +0300

    PDPS. Run integration tests without sudo

--- molecule/pdmysql/pdps/molecule/tests/test_orchestrator.py
+++ molecule/pdmysql/pdps/molecule/tests/test_orchestrator.py
@@ -50,9 +50,8 @@ def test_client(host):
 
 def test_integration(host):
     # command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';\""
-    with host.sudo():
-        # result = host.run(command)
-        # assert result.rc == 0, (result.stderr, result.stdout)
-        test_cmd = "cd /root/orchestrator/tests/integration && ./test.sh mysql"
-        test = host.run(test_cmd)
-        assert test.rc == 0, (test.stderr, test.stdout)
+    # result = host.run(command)
+    # assert result.rc == 0, (result.stderr, result.stdout)
+    test_cmd = "cd /root/orchestrator/tests/integration && ./test.sh mysql"
+    test = host.run(test_cmd)
+    assert test.rc == 0, test.stdout
