b2be390ec4db0ac487e402db7aeb6c6a993abc60
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-06-25 22:15:06 +0300

    PDPS. Add exec right to test script

--- molecule/pdmysql/pdps/molecule/tests/test_orchestrator.py
+++ molecule/pdmysql/pdps/molecule/tests/test_orchestrator.py
@@ -50,8 +50,9 @@ def test_client(host):
 
 def test_integration(host):
     # command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';\""
-    # result = host.run(command)
-    # assert result.rc == 0, (result.stderr, result.stdout)
-    test_cmd = "cd /root/orchestrator/tests/integration && ./test.sh mysql"
-    test = host.run(test_cmd)
-    assert test.rc == 0, (test.stderr, test.stdout)
+    with host.sudo():
+        # result = host.run(command)
+        # assert result.rc == 0, (result.stderr, result.stdout)
+        test_cmd = "cd /root/orchestrator/tests/integration && ./test.sh mysql"
+        test = host.run(test_cmd)
+        assert test.rc == 0, (test.stderr, test.stdout)
