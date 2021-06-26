9abf6586a54da1dc1f1db685339d3fe9364c7690
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-06-26 16:33:05 +0300

    PDPS. Exec as root all mysql statements for orchestrator sources test

--- molecule/pdmysql/tests/test_sources.py
+++ molecule/pdmysql/tests/test_sources.py
@@ -1,8 +1,8 @@
 def test_integration(host):
-    command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';\""
-    result = host.run(command)
-    assert result.rc == 0, (result.stderr, result.stdout)
     with host.sudo():
+        command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';\""
+        result = host.run(command)
+        assert result.rc == 0, (result.stderr, result.stdout)
         test_cmd = "cd /root/orchestrator/ && ./tests/integration/test.sh mysql"
         test = host.run(test_cmd)
         print(test.stdout)
