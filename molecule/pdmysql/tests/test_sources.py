209c44577ea2be0feeb95d9459b56232c750ec92
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-06-28 10:59:01 +0300

    PDPS. Run orchestror integration tests for centos

--- molecule/pdmysql/tests/test_sources.py
+++ molecule/pdmysql/tests/test_sources.py
@@ -1,8 +1,10 @@
 def test_integration(host):
     with host.sudo():
-        command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';\""
-        result = host.run(command)
-        assert result.rc == 0, (result.stderr, result.stdout)
+        dist = host.system_info.distribution
+        if dist.lower() not in ["redhat", "centos", 'rhel']:
+            command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';\""
+            result = host.run(command)
+            assert result.rc == 0, (result.stderr, result.stdout)
         test_cmd = "cd /root/orchestrator/ && ./tests/integration/test.sh mysql"
         test = host.run(test_cmd)
         print(test.stdout)
