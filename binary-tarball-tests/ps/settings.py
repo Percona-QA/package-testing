3d6a2d1f75379353d4aa1ab8e54ea44c3ca9cb9d
hrvojem <hmatijak@gmail.com>
2021-07-05 13:57:38 +0200

    Version changes

--- binary-tarball-tests/ps/settings.py
+++ binary-tarball-tests/ps/settings.py
@@ -39,15 +39,15 @@ ps80_functions = (
 ps80_files = (
   'lib/libcoredumper.a', 'lib/libHotBackup.so', 
   'lib/mysqlrouter/private/libmysqlrouter_http.so.1', 'lib/mysqlrouter/private/libmysqlrouter.so.1', 'lib/libmysqlservices.a',
-  'lib/libperconaserverclient.a', 'lib/libperconaserverclient.so.21.1.23' ,'lib/mysql/libjemalloc.so.1',
+  'lib/libperconaserverclient.a', 'lib/libperconaserverclient.so.21.1.25' ,'lib/mysql/libjemalloc.so.1',
   'lib/plugin/ha_tokudb.so', 'lib/plugin/ha_rocksdb.so', 'lib/plugin/audit_log.so',
   'lib/plugin/auth_pam.so', 'lib/plugin/auth_pam_compat.so', 'lib/plugin/data_masking.so',
   'lib/plugin/data_masking.ini', 'lib/plugin/tokudb_backup.so','lib/plugin/keyring_file.so',
   'lib/plugin/keyring_udf.so', 'lib/plugin/keyring_vault.so', 'lib/plugin/binlog_utils_udf.so'
 )
 ps80_symlinks = (
-  ('lib/libperconaserverclient.so.21','lib/libperconaserverclient.so.21.1.23'),
-  ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.21.1.23'),('lib/mysql/libjemalloc.so','lib/mysql/libjemalloc.so.1')
+  ('lib/libperconaserverclient.so.21','lib/libperconaserverclient.so.21.1.25'),
+  ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.21.1.25'),('lib/mysql/libjemalloc.so','lib/mysql/libjemalloc.so.1')
 )
 
 # 5.7
