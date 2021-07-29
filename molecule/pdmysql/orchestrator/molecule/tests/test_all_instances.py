28835fa49a4cbe4aabbb165bfe14f108bdb07533
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-07-29 11:28:00 +0300

    PDMYSQL. Orchestrator python tests for all instances

---
+++ molecule/pdmysql/orchestrator/molecule/tests/test_all_instances.py
@@ -0,0 +1,35 @@
+import os
+
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_all_instances(host):
+    cmd = "orchestrator-client -c all-instances | sort"
+    result = host.run(cmd)
+    assert result.rc == 0
+    print(result.stdout)
+    assert "127.0.0.1:10111" in result.stdout
+    assert "127.0.0.1:10112" in result.stdout
+    assert "127.0.0.1:10113" in result.stdout
+    assert "127.0.0.1:10114" in result.stdout
+
+
+def test_all_instances_replicas(host):
+    cmd = "orchestrator-client -c api -path all-instances | jq '.[] | .Replicas | length'"
+    result = host.run(cmd)
+    assert result.rc == 0
+    print(result.stdout)
+    result_stdout = result.stdout.split("\n")
+    print(result_stdout)
+    assert len(result_stdout) == 4, result_stdout
+    assert 3 in result_stdout, result_stdout
+    assert 0 in result_stdout, result_stdout
+    zero_cts = 0
+    for val in result_stdout:
+        if val == zero_cts:
+            zero_cts += 1
+    assert zero_cts == 3, result_stdout
