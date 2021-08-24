5ee00e768241100fb3bc9bf16db8919f51b64eff
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-08-24 10:08:45 +0300

    Orchestrator: Add redeploy action

--- molecule/pdmysql/orchestrator/molecule/tests/conftest.py
+++ molecule/pdmysql/orchestrator/molecule/tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import time
 
 import testinfra.utils.ansible_runner
 
@@ -8,5 +9,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 @pytest.fixture()
-def redeploy():
-    yield
+def redeploy(host):
+    with host.sudo("root"):
+        host.run_expect([0], "killall -9 mysqld_safe")
+        host.run_expect([0], "killall -9 mysqld")
+        host.run_expect([0], "killall -9 mysql")
+        time.sleep(5)
+        host.run_expect(
+            [0],
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/master/my.sandbox.cnf &",
+        )
+        host.run_expect(
+            [0],
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/node1/my.sandbox.cnf &",
+        )
+        host.run_expect(
+            [0],
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/node2/my.sandbox.cnf &",
+        )
+        host.run_expect(
+            [0],
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/node3/my.sandbox.cnf &",
+        )
+        time.sleep(10)
