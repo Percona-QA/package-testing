893778d9fba8ba354c2cee545189a8d918528d9e
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-08-24 11:26:08 +0300

    PMYSQL. Edit tests

--- molecule/pdmysql/orchestrator/molecule/tests/test_07_relocate_replicas.py
+++ molecule/pdmysql/orchestrator/molecule/tests/test_07_relocate_replicas.py
@@ -8,6 +8,32 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+@pytest.fixture()
+def redeploy(host):
+    with host.sudo("root"):
+        host.run_expect([0], "killall -9 mysqld_safe")
+        host.run_expect([0], "killall -9 mysqld")
+        host.run_expect([0], "killall -9 mysql")
+        time.sleep(5)
+        host.run_expect(
+            [0],
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/master/my.sandbox.cnf &",
+        )
+        host.run_expect(
+            [0],
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/node1/my.sandbox.cnf &",
+        )
+        host.run_expect(
+            [0],
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/node2/my.sandbox.cnf &",
+        )
+        host.run_expect(
+            [0],
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/node3/my.sandbox.cnf &",
+        )
+        time.sleep(10)
+
+
 @pytest.fixture(scope="module")
 def teardown(host):
     with host.sudo("root"):
