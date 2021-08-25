86f6020af6771f6525395ef7d96322c184d21dc1
Mikhail Samoylov <mikhail.samoylov@percona.com>
2021-08-25 23:15:03 +0300

    Orchestrator: Disable failed tests

--- molecule/pdmysql/orchestrator/molecule/tests/conftest.py
+++ molecule/pdmysql/orchestrator/molecule/tests/conftest.py
@@ -8,27 +8,63 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-# @pytest.fixture()
-# def redeploy(host):
-#     with host.sudo("root"):
-#         host.run_expect([0], "killall -9 mysqld_safe")
-#         host.run_expect([0], "killall -9 mysqld")
-#         host.run_expect([0], "killall -9 mysql")
-#         time.sleep(5)
-#         host.run_expect(
-#             [0],
-#             "mysqld_safe --defaults-file=/root/sandboxes/ci/master/my.sandbox.cnf &",
-#         )
-#         host.run_expect(
-#             [0],
-#             "mysqld_safe --defaults-file=/root/sandboxes/ci/node1/my.sandbox.cnf &",
-#         )
-#         host.run_expect(
-#             [0],
-#             "mysqld_safe --defaults-file=/root/sandboxes/ci/node2/my.sandbox.cnf &",
-#         )
-#         host.run_expect(
-#             [0],
-#             "mysqld_safe --defaults-file=/root/sandboxes/ci/node3/my.sandbox.cnf &",
-#         )
-#         time.sleep(10)
+def clean_data_dir(host):
+    pass
+
+
+def init_data_dir(host):
+    pass
+
+
+def create_ci_user(host):
+    pass
+
+
+def create_hearbeat_user(host):
+    pass
+
+
+def set_global_master_ro(host):
+    pass
+
+
+def create_test_db(host):
+    pass
+
+
+def configure_slaves(host):
+    pass
+
+
+def start_slaves(host):
+    pass
+
+
+
+
+
+@pytest.fixture()
+def redeploy(host):
+    with host.sudo("root"):
+        print("Killing mysql")
+        host.run("killall -9 mysqld_safe")
+        time.sleep(5)
+        print("Starting master")
+        host.run(
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/master/my.sandbox.cnf &",
+        )
+        print("Starting node1")
+        host.run(
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/node1/my.sandbox.cnf &",
+        )
+        print("Starting node2")
+
+        host.run(
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/node2/my.sandbox.cnf &",
+        )
+        print("Starting node3")
+        host.run(
+            "mysqld_safe --defaults-file=/root/sandboxes/ci/node3/my.sandbox.cnf &",
+        )
+        time.sleep(10)
+        print(host.run("ps aux | grep mysql").stdout)
--- molecule/pdmysql/orchestrator/molecule/tests_underwork/test_07_relocate_replicas.py
+++ molecule/pdmysql/orchestrator/molecule/tests_underwork/test_07_relocate_replicas.py
@@ -8,32 +8,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.fixture()
-def redeploy(host):
-    with host.sudo("root"):
-        print("Killing mysql")
-        host.run("killall -9 mysqld_safe")
-        time.sleep(5)
-        print("Starting master")
-        host.run(
-            "mysqld_safe --defaults-file=/root/sandboxes/ci/master/my.sandbox.cnf &",
-        )
-        print("Starting node1")
-        host.run(
-            "mysqld_safe --defaults-file=/root/sandboxes/ci/node1/my.sandbox.cnf &",
-        )
-        print("Starting node2")
-
-        host.run(
-            "mysqld_safe --defaults-file=/root/sandboxes/ci/node2/my.sandbox.cnf &",
-        )
-        print("Starting node3")
-        host.run(
-            "mysqld_safe --defaults-file=/root/sandboxes/ci/node3/my.sandbox.cnf &",
-        )
-        time.sleep(10)
-        print(host.run("ps aux | grep mysql").stdout)
-
 @pytest.fixture(scope="module")
 def teardown(host):
     with host.sudo("root"):
