7256ddaa329be8f7d03db9483cf954ea351c790d
Noemi Lapresta <noemi.lapresta@percona.com>
2021-09-10 16:29:20 +0200

    Modify ./run-local.sh for separate install and upgrade test flows

--- molecule/pxc/run-local.sh
+++ molecule/pxc/run-local.sh
@@ -2,30 +2,53 @@
 set -e
 
 function fail {
-  echo "Usage: ./run-local.sh version command"
+  echo "Usage: ./run-local.sh version test [action]"
   exit 1
 }
 
-command="$2"
-if [ -z "$command" ]; then fail; fi
+actions="${3:-"create converge destroy"}"
+test="$2"
+version="$1"
 
-case "$1" in
-  "80"|"57"|"56") version="$1" ;;
+case "$test" in
+  "install")
+    export install_repo="${test_repo:-"testing"}"
+    export check_version="yes" ;;
+  "upgrade")
+    export install_repo="main"
+    export check_version="no" ;;
   *)
-    echo 'Error: `version` must be one of [80, 57, 56]'
+    echo 'Error: `test` must be "install" or "upgrade"'
+    fail ;;
+esac
+
+case "$version" in
+  "80"|"57"|"56") ;;
+  *)
+    echo 'Error: `version` must be "80", "57" or "56"'
     fail ;;
 esac
 
 export TEST_DIST="${TEST_DIST:-"bento/ubuntu-20.04"}"
-export install_repo="testing"
 export PXC1_IP="192.168.70.151"
 export PXC2_IP="192.168.70.152"
 export PXC3_IP="192.168.70.153"
 
-cd "pxc$version-bootstrap"
-molecule "$command" -s vagrant
-cd -
+for action in $actions; do
+  cd "pxc$version-bootstrap"
+  molecule "$action" -s vagrant
+  cd -
 
-cd "pxc$version-common"
-molecule "$command" -s vagrant
-cd -
+  cd "pxc$version-common"
+  molecule "$action" -s vagrant
+  cd -
+
+  if [ "$test" == "upgrade" ] && [ "$action" == "converge" ]; then
+    export install_repo="${test_repo:-"testing"}"
+    export check_version="yes"
+
+    cd "pxc$version-bootstrap"
+    molecule "$action" -s vagrant
+    cd -
+  fi
+done
