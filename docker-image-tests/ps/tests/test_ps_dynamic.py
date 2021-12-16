40633e3ca278c77f7997de5c1a83c1b5e7a2ef08
Puneet Kaushik <puneet.kaushik@percona.com>
2021-12-16 16:18:18 +0530

    Add INIT_TOKUDB=1 to enable tokudb

--- docker-image-tests/ps/tests/test_ps_dynamic.py
+++ docker-image-tests/ps/tests/test_ps_dynamic.py
@@ -11,7 +11,7 @@ container_name = 'ps-docker-test-dynamic'
 @pytest.fixture(scope='module')
 def host():
     docker_id = subprocess.check_output(
-        ['docker', 'run', '--name', container_name, '-e', 'MYSQL_ROOT_PASSWORD='+ps_pwd, '-e', 'INIT_ROCKSDB=1','-d', docker_image]).decode().strip()
+        ['docker', 'run', '--name', container_name, '-e', 'MYSQL_ROOT_PASSWORD='+ps_pwd, '-e', 'INIT_TOKUDB=1', '-e', 'INIT_ROCKSDB=1','-d', docker_image]).decode().strip()
     time.sleep(20)
     yield testinfra.get_host("docker://root@" + docker_id)
 #    subprocess.check_call(['docker', 'rm', '-f', docker_id])
