9e97f25387e48c9a1213289c637c390d79a31d0b
Puneet Kala <puneet.0191@gmail.com>
2022-01-03 18:24:37 +0530

    PMM-7 Add check for custom query files (#122)
    * PMM-7 adding check task
    
    * PMM-7 Check Artifacts

---
+++ scripts/check_artifacts.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+## check exporters
+
+ls -la /usr/local/percona/pmm2/exporters | grep -q azure_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q mongodb_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q mysqld_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q node_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q postgres_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q proxysql_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q rds_exporter
+ls -la /usr/local/percona/pmm2/exporters | grep -q vmagent
+
+## check tools binary
+
+ls -la /usr/local/percona/pmm2/tools | grep -q pt-mongodb-summary
+ls -la /usr/local/percona/pmm2/tools | grep -q pt-mysql-summary
+ls -la /usr/local/percona/pmm2/tools | grep -q pt-pg-summary
+ls -la /usr/local/percona/pmm2/tools | grep -q pt-summary
+
+## check custom query files
+
+ls -la /usr/local/percona/pmm2/collectors/custom-queries/mysql/medium-resolution | grep -q "queries-mysqld.yml"
+ls -la /usr/local/percona/pmm2/collectors/custom-queries/mysql/high-resolution | grep -q "queries-mysqld.yml"
+ls -la /usr/local/percona/pmm2/collectors/custom-queries/mysql/high-resolution | grep -q "queries-mysqld-group-replication.yml"
+ls -la /usr/local/percona/pmm2/collectors/custom-queries/mysql/low-resolution | grep -q "queries-mysqld.yml"
+
+ls -la /usr/local/percona/pmm2/collectors/custom-queries/postgresql/high-resolution | grep -q "example-queries-postgres.yml"
+ls -la /usr/local/percona/pmm2/collectors/custom-queries/postgresql/high-resolution | grep -q "queries-postgres-uptime.yml"
+ls -la /usr/local/percona/pmm2/collectors/custom-queries/postgresql/medium-resolution | grep -q "example-queries-postgres.yml"
+ls -la /usr/local/percona/pmm2/collectors/custom-queries/postgresql/medium-resolution | grep -q "queries.yaml"
+ls -la /usr/local/percona/pmm2/collectors/custom-queries/postgresql/low-resolution | grep -q "example-queries-postgres.yml"
