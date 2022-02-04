42285eefef1e11e808e4f0d9a0e7c53f4d132ea6
Puneet Kala <puneet.0191@gmail.com>
2022-02-04 23:26:27 +0530

    PMM-9393 add support for metric check (#133)
    * PMM-9393 due to bug
    
    * PMM-9393 Adding support for metric verification
    
    * PMM-9393
    
    * PMM-9393 Adding check for exporter
    
    * pmm-9393 fix error
    
    * PMM-9393 Adding check in upgrade playbook
    
    * PMM-9393 Fix path
    
    * PMM-9393
    
    * PMM-9393 handle jq install
    
    * PMM-9393 Handle jq install
    
    * PMM-9393 Remove skipped check
    
    * PMM-9393

---
+++ scripts/pmm2_check_metric.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+   fi
+
+  shift
+done
+
+export service_id=$(pmm-admin list | grep ${service_name} | awk -F" " '{print $4}')
+if [ -z "${service_name}" ]; then
+        export agent_id=$(pmm-admin list | grep node_exporter | awk -F" " '{print $4}')
+else
+        export agent_id=$(pmm-admin list | grep ${service_id} | grep ${agent_type} | awk -F" " '{print $4}')
+fi
+
+if [ -z "$agent_password" ]; then
+        export agent_password=${agent_id}
+fi
+if [ -z "$pmm_client_ip" ]; then
+        export pmm_client_ip=127.0.0.1
+fi
+
+export token=$(printf '%s' pmm:${agent_password} | base64)
+export listen_port=$(curl -s "http://${pmm_server_ip}/v1/inventory/Agents/List" -H 'Authorization: Basic YWRtaW46YWRtaW4=' --data '{"promise":{}}' --compressed --insecure | jq ".${agent_type}[] | select(.agent_id == "\"${agent_id}"\") | .listen_port")
+if curl -s "http://${pmm_client_ip}:${listen_port}/metrics" | grep -q "${metric_name} ${metric_value}"; then
+        echo "Authentication for exporter Broken, metrics fetched without basic auth"
+        exit 1;
+fi
+curl -s -H "Authorization: Basic ${token}" "http://${pmm_client_ip}:${listen_port}/metrics" | grep "${metric_name} ${metric_value}"
