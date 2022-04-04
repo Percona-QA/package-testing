2cdfb72acd22d27f4270db7640f54d9e401a7fbd
Vadym Yarosh <vadym.yarosh@percona.com>
2022-04-04 13:01:23 +0300

    PMM-9732 config with port fix

--- scripts/pmm2_client_custom_path.sh
+++ scripts/pmm2_client_custom_path.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
 VERSION=${PMM_VERSION:-2.26.0}
+path=/pmm2-client-custom-path
+
+# Get the options
+while getopts ":hn:" option; do
+   case $option in
+      p) # Enter a custom path
+         path=$OPTARG;;
+      port) # custom port starts rom 2.27
+         VERSION=${PMM_VERSION:-2.27.0};;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+
 wget https://downloads.percona.com/downloads/TESTING/pmm/pmm2-client-${VERSION}.tar.gz
 tar -xvf pmm2-client-${VERSION}.tar.gz
 pushd /
-mkdir pmm2-client-custom-path
-pushd pmm2-client-custom-path
+mkdir -p ${path}
+pushd ${path}
 export PMM_DIR=$(pwd)
 popd
 popd
