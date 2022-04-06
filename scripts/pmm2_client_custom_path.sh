dd3d3dcfc13b3b978ea7dc6848a744e7c882fa1a
Vadym Yarosh <vadym.yarosh@percona.com>
2022-04-06 04:15:53 +0300

    PMM-9732 fixed script and absolute path usages. removed hardcoded port and flag.

--- scripts/pmm2_client_custom_path.sh
+++ scripts/pmm2_client_custom_path.sh
@@ -1,30 +1,58 @@
 #!/bin/bash
 
-VERSION=${PMM_VERSION:-2.26.0}
-path=/pmm2-client-custom-path
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   echo "Custom PMM 2 Client instalation script."
+   echo
+   echo "Syntax: scriptTemplate [-h|l|p]"
+   echo "options:"
+   echo "h     Print this Help."
+   echo "l     listening custom port mode. Sets default version to 2.27.0"
+   echo "p     Installation path. Default: /pmm2-client-custom-path"
+   echo
+}
 
-# Get the options
-while getopts ":hn:" option; do
+### Variables
+custom_path=/pmm2-client-custom-path
+version=${PMM_VERSION:-2.26.0}
+
+############################################################
+# Process the input options.                               #
+############################################################
+while getopts ":hlp:" option; do
    case $option in
+      h) # display Help
+        Help
+        exit 0
+        ;;
+      l) # listening custom port  starts from 2.27
+        echo "-l was triggered!"
+        version=${PMM_VERSION:-2.27.0}
+        ;;
       p) # Enter a custom path
-         path=$OPTARG;;
-      port) # custom port starts rom 2.27
-         VERSION=${PMM_VERSION:-2.27.0};;
+        path=$OPTARG
+        ;;
      \?) # Invalid option
          echo "Error: Invalid option"
-         exit;;
+         exit 1
+         ;;
    esac
 done
 
+echo "path='$custom_path', version='$version'"
 
-wget https://downloads.percona.com/downloads/TESTING/pmm/pmm2-client-${VERSION}.tar.gz
-tar -xvf pmm2-client-${VERSION}.tar.gz
+### Main program
+wget https://downloads.percona.com/downloads/TESTING/pmm/pmm2-client-${version}.tar.gz
+tar -xvf pmm2-client-${version}.tar.gz
 pushd /
-mkdir -p ${path}
-pushd ${path}
+mkdir -p ${custom_path}
+pushd ${custom_path}
 export PMM_DIR=$(pwd)
 popd
 popd
-mv pmm2-client-${VERSION} pmm2-client
+mv pmm2-client-${version} pmm2-client
 cd pmm2-client
 ./install_tarball
