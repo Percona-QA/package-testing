b38f4898d9d6fbe9426cf21b3fcf3ee062a3f69c
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-05-09 14:12:19 +0300

    Update lib names in settings.py for pxc5.7.37 tarball test

--- binary-tarball-tests/pxc/settings.py
+++ binary-tarball-tests/pxc/settings.py
@@ -101,7 +101,7 @@ pxc57_functions = (
   ('service_release_locks', 'locking_service.so', 'INT')
 )
 pxc57_files = (
-  'lib/libgalera_smm.so', 'lib/libperconaserverclient.a', 'lib/libperconaserverclient.so.20.3.23' ,
+  'lib/libgalera_smm.so', 'lib/libperconaserverclient.a', 'lib/libperconaserverclient.so.20.3.24' ,
   'lib/libmysqlservices.a' , 'lib/libcoredumper.a', 'lib/mysql/plugin/audit_log.so',
   'lib/mysql/plugin/auth_pam.so', 'lib/mysql/plugin/auth_pam_compat.so',
   'lib/mysql/plugin/keyring_file.so', 'lib/mysql/plugin/keyring_udf.so', 'lib/mysql/plugin/keyring_vault.so'
@@ -109,8 +109,8 @@ pxc57_files = (
 
 if glibc_version == "2.12":
   pxc57_symlinks = (
-    ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.20.3.23'),
-    ('lib/libperconaserverclient.so.20','lib/libperconaserverclient.so.20.3.23'),
+    ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.20.3.24'),
+    ('lib/libperconaserverclient.so.20','lib/libperconaserverclient.so.20.3.24'),
     ('lib/libcrypto.so','lib/private/libcrypto.so.1.0.1e'),
     ('lib/libssl.so','lib/private/libssl.so.1.0.1e'),
     ('lib/libtinfo.so','lib/private/libtinfo.so.5.7'),
@@ -119,8 +119,8 @@ if glibc_version == "2.12":
   )
 else:
   pxc57_symlinks = (
-    ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.20.3.23'),
-    ('lib/libperconaserverclient.so.20','lib/libperconaserverclient.so.20.3.23'),
+    ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.20.3.24'),
+    ('lib/libperconaserverclient.so.20','lib/libperconaserverclient.so.20.3.24'),
     ('lib/libncurses.so','lib/private/libncurses.so.5.9'),
     ('lib/libcrypto.so','lib/private/libcrypto.so.1.0.2k'),
     ('lib/libssl.so','lib/private/libssl.so.1.0.2k'),
