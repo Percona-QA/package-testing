71b0241fd73b7caf61375f0eabb8e78378937ef5
Puneet Kaushik <puneet.kaushik@percona.com>
2022-05-13 22:56:53 +0530

    Update JammyJellyfish

--- binary-tarball-tests/ps/test-ps-binary-tarball.groovy
+++ binary-tarball-tests/ps/test-ps-binary-tarball.groovy
@@ -115,6 +115,24 @@ pipeline {
     } //End stage Run tests
   } //End stages
 } //End pipeline
+        stage('Ubuntu JammyJellyfish') {
+          agent {
+            label "min-JammyJellyfish-x64"
+          }
+          steps {
+            script {
+                currentBuild.displayName = "#${BUILD_NUMBER}-${PS_VERSION}-${PS_REVISION}"
+              }
+            withCredentials([usernamePassword(credentialsId: 'JenkinsAPI', passwordVariable: 'JENKINS_API_PWD', usernameVariable: 'JENKINS_API_USER')]) {
+              run_test()
+            }
+            junit 'package-testing/binary-tarball-tests/ps/report.xml'
+          } //End steps
+        } //End stage JammyJellyfish
+      } //End parallel
+    } //End stage Run tests
+  } //End stages
+} //End pipeline
 
 void run_test() {
   sh '''
@@ -133,7 +151,7 @@ void run_test() {
     else
       sudo apt install -y git wget lsb-release
     fi
-    git clone https://github.com/Percona-QA/package-testing.git --branch master --depth 1
+    git clone https://github.com/kaushikpuneet07/package-testing.git --branch os-update --depth 1
     cd package-testing/binary-tarball-tests/ps
     wget -q ${TARBALL_LINK}${TARBALL_NAME}
     ./run.sh || true
