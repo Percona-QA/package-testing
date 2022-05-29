4467fb17ba37133dd39ad8ae6fa8fa940111f510
Puneet Kaushik <puneet.kaushik@percona.com>
2022-05-29 14:07:11 +0530

    Changes w.r.t PS 8.0.28-20

--- docker-image-tests/ps/tests/test_ps_static.py
+++ docker-image-tests/ps/tests/test_ps_static.py
@@ -15,7 +15,7 @@ def host():
     if ps_version_major in ['5.7','5.6']:
         subprocess.check_call(['docker','exec','--user','root',container_name,'microdnf','install','net-tools'])
     else:
-        subprocess.check_call(['docker','exec','--user','root',container_name,'microdnf','install','net-tools'])
+        subprocess.check_call(['docker','exec','--user','root',container_name,'yum','-y','install','net-tools'])
     time.sleep(20)
     yield testinfra.get_host("docker://root@" + docker_id)
     subprocess.check_call(['docker', 'rm', '-f', docker_id])
