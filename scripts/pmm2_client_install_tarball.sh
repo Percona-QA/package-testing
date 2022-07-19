065d8e706cc29df573aa9197fc5926737d8db527
Vadym Yarosh <vadym.yarosh@percona.com>
2022-07-19 15:50:19 +0200

    PMM-10173  added and applied new tarbal install script

---
+++ scripts/pmm2_client_install_tarball.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   echo "Custom PMM 2 Client installation script. To handle custom installations path and"
+   echo "To handle custom installation path and port listening tests"
+   echo
+   echo "Syntax: pmm2_client_install_tarball.sh [-v X.XX.X] [-p /my/path] [-h|l]"
+   echo "options:"
+   echo "h     Print this Help."
+   echo "v     Installing specified version 2.XX.X"
+   echo "p     Installation path. Default: /usr/local/percona/pmm2."
+   echo "      Sets default version to 2.27.0 if no version specified"
+   echo "l     listening custom port mode. Sets default version to 2.27.0 if no version specified"
+}
+
+### Defaults
+default_path=/usr/local/percona/pmm2
+
+# custom path flag and installation are supported from 2.26.0
+default_version=2.26.0
+min_port_listening_version=2.27.0
+port_listening=0
+
+############################################################
+# Process the input options.                               #
+############################################################
+while getopts "v:p:hl" option; do
+   case $option in
+      h) # display Help
+        Help
+        exit 0
+        ;;
+      v) # Enter a version
+        if [ -n "$OPTARG" ]
+        then
+          version=$OPTARG
+        fi
+        ;;
+      p) # Enter a custom path
+        if [ -n "$OPTARG" ]
+        then
+          path=$OPTARG
+        fi
+        ;;
+      l) # listening custom port starts from 2.27.0
+        port_listening=1
+        ;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit 1
+         ;;
+   esac
+done
+
+############################################################
+# Post-process args, to ignore the order of the flags      #
+############################################################
+if [[ -z "${version}" ]]; then
+  echo "version is '${version}'"
+  if [[ ${port_listening} == 1 ]]; then
+    echo ${min_port_listening_version}
+    version=${min_port_listening_version}
+  else
+    version=${default_version}
+  fi
+else
+  min_ver=$(echo $version | awk -F'.' '{print $2}')
+  if [[ $port_listening = 1 && $min_ver -lt 27 ]]; then
+    echo "listen-port is not available for versions earlier 2.27.0!" >&2;
+    exit 1
+  fi
+  if [[ $min_ver -lt 26 && -n "${path}" && ${path} != "${default_path}" ]]; then
+    echo "pmm2-client setup in custom folder is not available for versions earlier 2.26.0!" >&2;
+    exit 1
+  fi
+fi
+if [ -z "${path}" ]; then
+  path=$default_path
+fi
+tarball_url=https://downloads.percona.com/downloads/TESTING/pmm/pmm2-client-${version}.tar.gz
+
+### Main program
+echo "Downloading ${tarball_url}"
+mkdir -p ./tmp/
+#wget ${tarball_url} -nv -P ./tmp/
+wget ${tarball_url} -P ./tmp/
+tar -xvf ./tmp/pmm2-client-${version}.tar.gz -C ./tmp/
+echo "Installing tarball to ${path}"
+mkdir -p ${path}
+export PMM_DIR=${path}
+echo $PMM_DIR
+### uncomment when PMM-10247 will be merged
+#if [[ $min_ver -lt 30 ]]; then
+  cd ./tmp/pmm2-client-${version}
+  ./install_tarball
+  cd ../../
+#else
+#  ./tmp/pmm2-client-${version}/install_tarball
+#fi
+ln -sf ${path}/bin/pmm-admin /usr/bin/pmm-admin
+ln -sf ${path}/bin/pmm-agent /usr/bin/pmm-agent
+echo 'Done!'
