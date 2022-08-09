bf03d116a7599830374c7983e9ed93b56800464a
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-08-09 15:43:20 +0300

    Update pdps-setup with 3 rpm orchestrator packages (#238)

--- molecule/pdmysql/pdps-setup/molecule/tests/test_orchestrator.py
+++ molecule/pdmysql/pdps-setup/molecule/tests/test_orchestrator.py
@@ -7,27 +7,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-DEBPACKAGES = ['percona-orchestrator-cli', 'percona-orchestrator-client', 'percona-orchestrator']
+PACKAGES = ['percona-orchestrator-cli', 'percona-orchestrator-client', 'percona-orchestrator']
+
+VERSION = os.getenv("ORCHESTRATOR_VERSION")
 
 
-@pytest.mark.parametrize("package", DEBPACKAGES)
+@pytest.mark.parametrize("package", PACKAGES)
 def test_check_deb_package(host, package):
-    dist = host.system_info.distribution
-    if dist.lower() in ["redhat", "centos", 'rhel']:
-        pytest.skip("This test only for Debian based platforms")
     pkg = host.package(package)
     assert pkg.is_installed
-    assert '3.1.4' in pkg.version, pkg.version
-
-
-def test_check_rpm_package(host):
-    dist = host.system_info.distribution
-    if dist.lower() in ["debian", "ubuntu"]:
-        pytest.skip("This test only for RHEL based platforms")
-    pkg = host.package('percona-orchestrator')
-    assert pkg.is_installed
-    assert '3.1.4' in pkg.version, pkg.version
-
+    assert VERSION in pkg.version, pkg.version
 
 def test_orchestrator_version(host):
     cmd = 'orchestrator --version'
@@ -39,9 +28,6 @@ def test_orchestrator_version(host):
 
 
 def test_orchestrator_client(host):
-    cmd = 'orchestrator-client --help'
-    dist = host.system_info.distribution
-    if dist.lower() in ["redhat", "centos", 'rhel']:
-        cmd = "/usr/local/orchestrator/resources/bin/orchestrator-client --help"
+    cmd = 'orchestrator-client --help h'
     result = host.run(cmd)
     assert result.rc == 0, result.stderr
