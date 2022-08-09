e194418e80b6fa4d04f18fca07ee020a0d0b732c
Puneet Kaushik <87129594+kaushikpuneet07@users.noreply.github.com>
2022-08-09 16:52:35 +0530

    Enable mysql-shell (#236)

--- version_check.sh
+++ version_check.sh
@@ -108,14 +108,14 @@ if [ "${product}" = "ps56" -o "${product}" = "ps57" -o "${product}" = "ps80" ]; 
     exit 1
   fi
 
-#  if [ ${product} = "ps80" ]; then
-#    if [ "$(mysqlsh --version | grep -c ${version})" = 1 ]; then
-#      echo "mysql-shell version is correct" >> "${log}"
-#    else
-#      echo "ERROR: mysql-shell version is incorrect"
-#      exit 1
-#    fi
-#  fi
+  if [ ${product} = "ps80" ]; then
+    if [ "$(mysqlsh --version | grep -c ${version})" = 1 ]; then
+      echo "mysql-shell version is correct" >> "${log}"
+    else
+      echo "ERROR: mysql-shell version is incorrect"
+      exit 1
+    fi
+  fi
 
 elif [ ${product} = "pxc56" -o ${product} = "pxc57" -o ${product} = "pxc80" ]; then
   for i in @@INNODB_VERSION @@VERSION; do
