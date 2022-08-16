d58bf761e9ce2d008f9e9c32a6ea7e8bdfb62895
panchal-yash <yash.panchal@percona.com>
2022-08-16 19:32:10 +0530

    Modify the log warning check script for simple output

--- scripts/log-warning-check.sh
+++ scripts/log-warning-check.sh
@@ -3,9 +3,9 @@
 logfile=$1
 
 if [ "$(egrep -c "WARNING" ${logfile})" != 0 ];then
-        echo "WARNING: Warnings or Errors found in the installation logs:\n"
-        egrep "WARNING" ${logfile}
+        echo "!!! WARNING Detected !!!"
         exit 1
 else
         echo "Installation log is clean"
 fi
+
