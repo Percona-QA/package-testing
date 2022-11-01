c125eb04dadea51edbfb6540f981885c1b2b1f90
Puneet Kaushik <puneet.kaushik@percona.com>
2022-11-02 01:24:03 +0530

    Update password

--- molecule/pdmysql/tests/test_sources.py
+++ molecule/pdmysql/tests/test_sources.py
@@ -2,7 +2,7 @@ def test_integration(host):
     with host.sudo():
         dist = host.system_info.distribution
         if dist.lower() not in ["redhat", "centos", "rhel", "oracleserver","ol"]:
-            command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';\""
+            command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Percona@12345';\""
             result = host.run(command)
             assert result.rc == 0, (result.stderr, result.stdout)
         test_cmd = "cd /root/orchestrator/ && ./tests/integration/test.sh mysql"
