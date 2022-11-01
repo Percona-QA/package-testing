5473b596574aec1d1c1e550d64040744c91dd026
Puneet Kaushik <puneet.kaushik@percona.com>
2022-11-02 02:04:32 +0530

    update amazon in tests

--- molecule/pdmysql/tests/test_sources.py
+++ molecule/pdmysql/tests/test_sources.py
@@ -1,7 +1,7 @@
 def test_integration(host):
     with host.sudo():
         dist = host.system_info.distribution
-        if dist.lower() not in ["redhat", "centos", "rhel", "oracleserver","ol"]:
+        if dist.lower() not in ["redhat", "centos", "rhel", "oracleserver", "ol", "amazon"]:
             command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Percona@12345';\""
             result = host.run(command)
             assert result.rc == 0, (result.stderr, result.stdout)
