efc5ac0c447da7460818ff7a80c4bfa2841bdc19
Puneet Kaushik <puneet.kaushik@percona.com>
2022-11-02 22:25:50 +0530

    fix test_sources.py

--- molecule/pdmysql/tests/test_sources.py
+++ molecule/pdmysql/tests/test_sources.py
@@ -1,7 +1,7 @@
 def test_integration(host):
     with host.sudo():
         dist = host.system_info.distribution
-        if dist.lower() not in ["redhat", "centos", "rhel", "oracleserver", "ol", "amazon"]:
+        if dist.lower() not in ["redhat", "centos", "rhel", "oracleserver", "ol", "amazonlinux", "amzn"]:
             command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';\""
             result = host.run(command)
             assert result.rc == 0, (result.stderr, result.stdout)
