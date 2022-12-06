a4fb71ffdbc5a08fa5d85895f0df736bf7024d47
Puneet Kaushik <puneet.kaushik@percona.com>
2022-12-06 17:04:01 +0530

    Update Buster

--- binary-tarball-tests/ps/test-ps-binary-tarball-56.groovy
+++ binary-tarball-tests/ps/test-ps-binary-tarball-56.groovy
@@ -41,6 +41,33 @@ pipeline {
             junit 'package-testing/binary-tarball-tests/ps/report.xml'
           } //End steps
         } //End stage Ubuntu Bionic
+        stage('Buster') {
+          agent {
+            label "min-buster-x64"
+          }
+          steps {
+            withCredentials([usernamePassword(credentialsId: 'JenkinsAPI', passwordVariable: 'JENKINS_API_PWD', usernameVariable: 'JENKINS_API_USER')]) {
+              sh '''
+                PS_MAJOR_VERSION="$(echo ${PS_VERSION}|cut -d'.' -f1,2)"
+                MINIMAL=""
+                if [ BUILD_TYPE_MINIMAL ]; then
+                  MINIMAL="-minimal"
+                fi
+                if [ "${PS_MAJOR_VERSION}" = "5.6" ]; then
+                  TARBALL_NAME="Percona-Server-5.6.51-rel93.0-Linux.x86_64.ssl100.tar.gz"
+                  TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/ps-5.6.51-93/"
+                fi
+                rm -rf package-testing
+                sudo apt install -y git wget
+                git clone https://github.com/kaushikpuneet07/package-testing.git --branch ps-5.6 --depth 1
+                cd package-testing/binary-tarball-tests/ps
+                wget -q ${TARBALL_LINK}${TARBALL_NAME}
+                ./run.sh || true
+              '''
+            }
+            junit 'package-testing/binary-tarball-tests/ps/report.xml'
+          } //End steps
+        } //End stage Centos7
         stage('Centos7') {
           agent {
             label "min-centos-7-x64"
