8c1bdf3e1db776198e15eda39e8c95a058ed2e60
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-12-13 11:55:03 +0200

    DISTMYSQL-221: use single tests folder for PDPXC and PDPS tests. (#311)

---
+++ molecule/pdmysql/tests/test_common_pxb_pt.py
@@ -0,0 +1,61 @@
+import os
+import pytest
+import testinfra.utils.ansible_runner
+from .settings import *
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+DEBPACKAGES = ['percona-xtrabackup-80',
+               'percona-xtrabackup-test-80',
+               'percona-xtrabackup-dbg-80']
+
+RPMPACKAGES = ['percona-xtrabackup-80',
+               'percona-xtrabackup-test-80',
+               'percona-xtrabackup-80-debuginfo']
+
+PTBINS = ['pt-align', 'pt-archiver', 'pt-config-diff', 'pt-deadlock-logger', 'pt-diskstats',
+          'pt-duplicate-key-checker', 'pt-fifo-split', 'pt-find', 'pt-fingerprint',
+          'pt-fk-error-logger', 'pt-heartbeat', 'pt-index-usage', 'pt-ioprofile', 'pt-kill',
+          'pt-mext', 'pt-mongodb-query-digest', 'pt-mongodb-summary', 'pt-mysql-summary',
+          'pt-online-schema-change', 'pt-pmp', 'pt-query-digest', 'pt-show-grants', 'pt-sift',
+          'pt-slave-delay', 'pt-slave-find', 'pt-slave-restart', 'pt-stalk', 'pt-summary',
+          'pt-table-checksum', 'pt-table-sync', 'pt-table-usage', 'pt-upgrade',
+          'pt-variable-advisor', 'pt-visual-explain']
+
+PXB_VERSION = os.getenv("PXB_VERSION")
+PT_VERSION = os.getenv("PT_VERSION")
+
+
+@pytest.mark.parametrize("package", DEBPACKAGES)
+def test_check_deb_package(host, package):
+    dist = host.system_info.distribution
+    if dist.lower() in RHEL_DISTS:
+        pytest.skip("This test only for RHEL based platforms")
+    pkg = host.package(package)
+    assert pkg.is_installed
+    assert PXB_VERSION in pkg.version, pkg.version
+
+
+@pytest.mark.parametrize("package", RPMPACKAGES)
+def test_check_rpm_package(host, package):
+    dist = host.system_info.distribution
+    if dist.lower() in DEB_DISTS:
+        pytest.skip("This test only for RHEL based platforms")
+    pkg = host.package(package)
+    assert pkg.is_installed
+    assert PXB_VERSION in pkg.version, pkg.version
+
+
+def test_binary_version(host):
+    cmd = "xtrabackup --version"
+    result = host.run(cmd)
+    assert result.rc == 0, result.stderr
+    assert PXB_VERSION in result.stderr, (result.stdout, result.stdout)
+
+
+@pytest.mark.parametrize("pt_bin", PTBINS)
+def test_pt_binaries(host, pt_bin):
+    cmd = '{} --version'.format(pt_bin)
+    result = host.run(cmd)
+    assert PT_VERSION in result.stdout, result.stdout
---
+++ molecule/pdmysql/tests/test_pdpxc_haproxy.py
@@ -0,0 +1,32 @@
+import os
+import pytest
+import testinfra.utils.ansible_runner
+from .settings import *
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+DEBPACKAGES = ['percona-haproxy', 'percona-haproxy-doc', 'percona-vim-haproxy']
+
+RPMPACKAGES = ['percona-haproxy', 'percona-haproxy-debuginfo']
+HAPROXY_VERSION = os.environ.get("HAPROXY_VERSION")
+
+
+@pytest.mark.parametrize("package", DEBPACKAGES)
+def test_check_deb_package(host, package):
+    dist = host.system_info.distribution
+    if dist.lower() in RHEL_DISTS:
+        pytest.skip("This test only for Debian based platforms")
+    pkg = host.package(package)
+    assert pkg.is_installed
+    assert HAPROXY_VERSION in pkg.version, pkg.version
+
+
+@pytest.mark.parametrize("package", RPMPACKAGES)
+def test_check_rpm_package(host, package):
+    dist = host.system_info.distribution
+    if dist.lower() in DEB_DISTS:
+        pytest.skip("This test only for RHEL based platforms")
+    pkg = host.package(package)
+    assert pkg.is_installed
+    assert HAPROXY_VERSION in pkg.version, pkg.version
