0c838a60382d6d5de42474748ca464c1f26dfd43
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2022-12-21 13:12:15 +0200

    Remome commented test

--- molecule/pdmysql/tests/test_pdpxc_repl_manager.py
+++ molecule/pdmysql/tests/test_pdpxc_repl_manager.py
@@ -1,7 +1,6 @@
 import os
 import pytest
 import testinfra.utils.ansible_runner
-from .settings import *
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
@@ -10,19 +9,8 @@ PACKAGES = ['percona-replication-manager']
 
 REPL_MANAGER_VERSION = os.environ.get("REPL_MANAGER_VERSION")
 
-
 @pytest.mark.parametrize("package", PACKAGES)
 def test_check_package(host, package):
     pkg = host.package(package)
     assert pkg.is_installed
     assert REPL_MANAGER_VERSION in pkg.version, pkg.version
-
-
-# @pytest.mark.parametrize("package", PACKAGES)
-# def test_check_rpm_package(host, package):
-#     dist = host.system_info.distribution
-#     if dist.lower() in DEB_DISTS:
-#         pytest.skip("This test only for RHEL based platforms")
-#     pkg = host.package(package)
-#     assert pkg.is_installed
-#     assert REPL_MANAGER_VERSION in pkg.version, pkg.version
