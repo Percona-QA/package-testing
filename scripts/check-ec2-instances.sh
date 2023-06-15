e2ddf84cfb202621909f60b2406f70490be29a4b
Yash Panchal <46398416+panchal-yash@users.noreply.github.com>
2023-06-15 14:26:38 +0530

    Update check-ec2-instances.sh
    Add proper regions
    Add job-name Tag under query : empty if job-name is null

--- scripts/check-ec2-instances.sh
+++ scripts/check-ec2-instances.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
 ## Calculate the threshold date (1 day ago)
-awsRegions=("us-west-1" "us-west-2" "us-east-1" "us-east-2" "eu-west-1" "eu-west-2" "eu-east-1" "us-east-2")
+awsRegions=("us-west-1" "us-west-2" "us-east-1" "us-east-2" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "eu-central-1")
 
 
 checkec2(){
           threshold_date=$(date -d "$2 day ago" +%Y-%m-%d)
 
-            aws ec2 describe-instances     --filters "Name=instance-state-name,Values=running"     --query "Reservations[].Instances[?LaunchTime<='${threshold_date}'].[Tags[?Key=='Name'].Value[], InstanceId, LaunchTime]" --output yaml --region $1 | sed -e 's/\- \[\]//g' -e '/^$/d'
+           aws ec2 describe-instances --filters "Name=instance-state-name,Values=running"     --query "Reservations[].Instances[?LaunchTime<='${threshold_date}'].[Tags[?Key=='Name'].Value[],Tags[?Key=='job-name'].Value[], InstanceId, LaunchTime]" --output yaml --region $1 | sed -e 's/\- \[\]//g' -e '/^$/d'
+
     }
 
 
