a161bce69fb40a8488ad9733f8184b472531e2c5
Puneet Kaushik <puneet.kaushik@percona.com>
2023-08-07 18:32:28 +0530

    Skipping for Perconalab

--- docker-image-tests/pxc/tests/test_pxc_major.py
+++ docker-image-tests/pxc/tests/test_pxc_major.py
@@ -5,7 +5,7 @@ import testinfra
 import time
 from settings import *
 
-
+docker_acc = os.getenv('DOCKER_ACC')
 container_name = 'pxc-docker-test-static1'
 
 @pytest.fixture(scope='module')
@@ -21,18 +21,21 @@ def host():
     yield testinfra.get_host("docker://root@" + docker_id)
     subprocess.check_call(['docker', 'rm', '-f', docker_id])
 
+@pytest.mark.skipif(docker_acc == "perconalab", reason="Skipping tests in 'testing' repo")
 class TestMysqlEnvironment:
     @pytest.mark.parametrize("pkg_name,pkg_version", pxc_packages)
     def test_packages(self, host, pkg_name, pkg_version):
         assert host.package(pkg_name).is_installed
         assert host.package(pkg_name).version == pkg_version
 
+    @pytest.mark.skipif(docker_acc == "perconalab", reason="Skipping tests in 'testing' repo")
     def test_mysql_version(self, host):
         if pxc_version_major in ['5.7','5.6']:
             assert host.check_output('mysql --version') == 'mysql  Ver 14.14 Distrib '+pxc57_client_version+', for Linux (x86_64) using  7.0'
         else:
             assert host.check_output('mysql --version') == 'mysql  Ver '+ pxc_version +' for Linux on x86_64 (Percona XtraDB Cluster (GPL), Release rel'+ pxc_rel +', Revision '+ pxc_revision +', WSREP version '+ pxc_wsrep_version +')'
 
+    @pytest.mark.skipif(docker_acc == "perconalab", reason="Skipping tests in 'testing' repo")
     def test_mysqld_version(self, host):
         if pxc_version_major in ['5.7','5.6']:
             assert host.check_output('mysqld --version') == 'mysqld  Ver '+pxc57_server_version_norel+' for Linux on x86_64 (Percona XtraDB Cluster (GPL), Release '+pxc57_server_release+', Revision '+pxc_revision+', WSREP version '+ pxc_wsrep_version +', wsrep_'+ pxc_wsrep_version +')'
