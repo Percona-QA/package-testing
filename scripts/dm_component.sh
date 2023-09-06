fd014848b8a57a32e214094a19afdddcd5a26150
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-09-06 23:27:55 +0300

    Ready DM component test

--- scripts/dm_component.sh
+++ scripts/dm_component.sh
@@ -7,13 +7,16 @@ if [ ${DM_PLUGIN} == 1 ]; then
    echo "DM plugin is installed and active"
    exit 1
 else
-   echo "ERROR: DM plugin isn't installed or active"
+   echo "DM plugin isn't installed or active"
 fi
 
-set -e
-
 #install the data masking 
-mysql -uroot -S/tmp/mysql_24000.sock -NBe "INSTALL COMPONENT 'file://component_masking';"
+install=$(mysql -uroot -S/tmp/mysql_24000.sock -NBe "INSTALL COMPONENT 'file://component_masking_functions';")
+install_result=$?
+if [[ "${install_result}" == '1' ]]; then
+    echo "Exiting because there was failure during component install!"
+    exit 1
+fi
 
 # make sure DM plugin is active
 DM_COMPONENT=$(mysql -uroot -S/tmp/mysql_24000.sock -NBe "select * from mysql.component;" | grep -c component_masking_functions)
@@ -33,7 +36,7 @@ mysql -uroot -S/tmp/mysql_24000.sock -NBe "GRANT MASKING_DICTIONARIES_ADMIN on *
 
 
 
-#use the Data Masking functions
+#Create lists of: static UDFs (we know expected result), reference list for static and random UDFs
 staticUDFs=("SELECT mask_canada_sin('046454286A');" "SELECT mask_canada_sin('046454286A', '#');" \
          "SELECT mask_iban('LC14BOSL12345678901234567890123422');" "SELECT mask_iban('LC14BOSL12345678901234567890123422', '#');" \
          "SELECT mask_inner('This is a string', 4, 1);" "SELECT mask_inner('This is a string', 1, 4);" \
@@ -62,35 +65,43 @@ referenceList=("XXXXXXXXXX" "##########" \
          "1" "1" \
          "1" "test_term2_1" \
          "test_term2_1" "1" )
-# randomUDFs=("SELECT gen_range(1, 10);" "SELECT gen_rnd_canada_sin();" \
-#          "SELECT gen_rnd_email();" "SELECT gen_rnd_email(3,4,'mydomain.com');" \
-#          "SELECT gen_rnd_iban();" "SELECT gen_rnd_iban('UA',20);" \
-#          "SELECT gen_rnd_pan();" "SELECT gen_rnd_ssn();" \
-#          "SELECT gen_rnd_uk_nin();" "SELECT gen_rnd_us_phone();" \
-#          "SELECT gen_rnd_uuid();")
+randomUDFs=("SELECT gen_range(1, 10);" "SELECT gen_rnd_canada_sin();" \
+         "SELECT gen_rnd_email();" "SELECT gen_rnd_email(3,4,'mydomain.com');" \
+         "SELECT gen_rnd_iban();" "SELECT gen_rnd_iban('UA',20);" \
+         "SELECT gen_rnd_pan();" "SELECT gen_rnd_ssn();" \
+         "SELECT gen_rnd_uk_nin();" "SELECT gen_rnd_us_phone();" \
+         "SELECT gen_rnd_uuid();")
+
+#Check UDFs results of which we know by comparing UDF result to reference value
 for i in ${!staticUDFs[@]}; do 
     result=$(mysql -uroot -S/tmp/mysql_24000.sock -NBe "${staticUDFs[$i]}")
-    if [ "${result}" == "${referenceList[$i]}" ]; then
-        echo 'Equal'
-    else
+    if [[ "${result}" != "${referenceList[$i]}" ]]; then
         echo "${staticUDFs[$i]} result is incorrect. Current result is: ${result}. Expected result is: ${referenceList[$i]}"
         fails='1'
     fi
 done
-# for i in ${randomUDFs[@]}; do
 
-#     result=$(mysql -uroot -S/tmp/mysql_24000.sock -NBe "${i}")
-#     if [[ -z "${result}" ]]; then
-#         fails='2'
-#         echo "There is and error"
-#     fi
-#     echo ${i} has result ${result}
-# done
-# Disable Data Masking plugin
-mysql -uroot -S/tmp/mysql_24000.sock -NBe "UNINSTALL PLUGIN data_masking';
+#Check UDFs that generate random result. MySQL ERRORs have empty result.
+for i in "${randomUDFs[@]}"; do
+    result=$(mysql -uroot -S/tmp/mysql_24000.sock -NBe "${i}")
+    if [[ -z "${result}" ]]; then
+        fails='1'
+        echo "${i} has empty result."
+    fi
+done
 
+# set -e
+# Disable Data Masking component
+mysql -uroot -S/tmp/mysql_24000.sock -NBe "DROP TABLE IF EXISTS mysql.masking_dictionaries;"
+uninstall=$(mysql -uroot -S/tmp/mysql_24000.sock -NBe "UNINSTALL COMPONENT 'file://component_masking_functions';")
+uninstall_result=$?
+
+#Exit script with and error if any of the checks failed.
 if [[ "${fails}" == '1' ]]; then
     echo "Exiting because there were failed UDFs!"
+    exit 1
+elif [[ "${uninstall_result}" == '1' ]]; then
+    echo "Exiting because there was failure during component uninstall!"
     exit 1
 fi
 
