d47819a010a68a1dfbcdcf0629bc94006c0603cc
Puneet Kaushik <87129594+kaushikpuneet07@users.noreply.github.com>
2023-09-16 14:09:31 +0530

    Fix link to download tarball (#533)

--- binary-tarball-tests/ps/test-ps-binary-tarball.groovy
+++ binary-tarball-tests/ps/test-ps-binary-tarball.groovy
@@ -147,11 +147,7 @@ void run_test() {
       fi
     fi
     TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.glibc${GLIBC_VERSION}${MINIMAL}.tar.gz"
-    if [ "${PS_MAJOR_VERSION}" = "8.0" ]; then
-      TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/ps-release-${PS_VERSION}/"
-    else
-      TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/ps-${PS_VERSION}/"
-    fi
+    TARBALL_LINK="https://downloads.percona.com/downloads/TESTING/ps-${PS_VERSION}/"
     rm -rf package-testing
     git clone https://github.com/Percona-QA/package-testing.git --branch master --depth 1
     cd package-testing/binary-tarball-tests/ps
