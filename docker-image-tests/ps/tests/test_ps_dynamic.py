64c9075cdce2a26686266a94c5506eba5d5b5f0b
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2023-11-08 15:12:57 +0200

    Send ps telemetry to dev server

--- docker-image-tests/ps/tests/test_ps_dynamic.py
+++ docker-image-tests/ps/tests/test_ps_dynamic.py
@@ -12,10 +12,10 @@ container_name = 'ps-docker-test-dynamic'
 def host():
     if ps_version_major not in ['8.0']:
         docker_id = subprocess.check_output(
-        ['docker', 'run', '--name', container_name, '-e', 'MYSQL_ROOT_PASSWORD='+ps_pwd, '-e', 'INIT_TOKUDB=1', '-e', 'INIT_ROCKSDB=1','-d', docker_image]).decode().strip()
+        ['docker', 'run', '--name', container_name, '-e', 'MYSQL_ROOT_PASSWORD='+ps_pwd, '-e', 'INIT_TOKUDB=1', '-e', 'INIT_ROCKSDB=1', '-e', 'PERCONA_TELEMETRY_URL=https://check-dev.percona.com/v1/telemetry/GenericReport','-d', docker_image]).decode().strip()
     else:
         docker_id = subprocess.check_output(
-        ['docker', 'run', '--name', container_name, '-e', 'MYSQL_ROOT_PASSWORD='+ps_pwd, '-e', 'INIT_ROCKSDB=1','-d', docker_image]).decode().strip()
+        ['docker', 'run', '--name', container_name, '-e', 'MYSQL_ROOT_PASSWORD='+ps_pwd, '-e', 'INIT_ROCKSDB=1', '-e', 'PERCONA_TELEMETRY_URL=https://check-dev.percona.com/v1/telemetry/GenericReport', '-d', docker_image]).decode().strip()
     time.sleep(20)
     yield testinfra.get_host("docker://root@" + docker_id)
 #    subprocess.check_call(['docker', 'rm', '-f', docker_id])
