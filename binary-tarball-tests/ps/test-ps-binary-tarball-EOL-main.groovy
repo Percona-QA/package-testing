a390a9d47b7277bbbf1d93f0ea23048473a60d1f
Puneet Kaushik <puneet.kaushik@percona.com>
2024-03-14 20:56:03 +0530

    Tetsing tarball job

--- binary-tarball-tests/ps/test-ps-binary-tarball-EOL-main.groovy
+++ binary-tarball-tests/ps/test-ps-binary-tarball-EOL-main.groovy
@@ -3,9 +3,11 @@ pipeline {
         label 'docker'
     }
     parameters {
-        string(name: 'PS_VERSION', defaultValue: '8.0.33-25', description: 'PS full version')
-        string(name: 'PS_REVISION', defaultValue: '9468fd1d', description: 'PS revision')
+        string(name: 'PS_VERSION', defaultValue: '5.7.44-49', description: 'PS full version')
+        string(name: 'PS_REVISION', defaultValue: 'c643a1242d8', description: 'PS revision')
+        choice(name: 'REPO', choices: ['testing','main'], description: testing or main)
         booleanParam(defaultValue: false, name: 'BUILD_TYPE_MINIMAL')
+        booleanParam(defaultValue: false, name: 'BUILD_TYPE_DEBUG')
     }
     stages {
         stage('Binary tarball test') {
@@ -16,7 +18,7 @@ pipeline {
                     }
                     steps {
                         script {
-                            currentBuild.displayName = "#${BUILD_NUMBER}-${PS_VERSION}-${PS_REVISION}"
+                            currentBuild.displayName = "#${BUILD_NUMBER}-${PS_VERSION}-${PS_REVISION}-${REPO}"
                         }
                         withCredentials([usernamePassword(credentialsId: 'PS_PRIVATE_REPO_ACCESS', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
                             sh '''
@@ -25,13 +27,21 @@ pipeline {
                                 if [ "${BUILD_TYPE_MINIMAL}" = "true" ]; then
                                     MINIMAL="-minimal"
                                 fi
+                                echo ${BUILD_TYPE_DEBUG}
+                                DEBUG=""
+                                if [ "${BUILD_TYPE_DEBUG}" = "true" ]; then
+                                    DEBUG="-debug"
+                                fi
                                 if [ -f /usr/bin/yum ]; then
                                     sudo yum install -y git wget
                                 else
                                     sudo apt install -y git wget
                                 fi
-                                TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.jammy${MINIMAL}.tar.gz"
+                                TARBALL_NAME="Percona-Server-${PS_VERSION}-Linux.x86_64.jammy${MINIMAL}{DEBUG}.tar.gz"
+                                if (REPO == 'main')
                                 TARBALL_LINK="https://repo.percona.com/private/${USERNAME}-${PASSWORD}/ps-57-eol/tarballs/Percona-Server-${PS_VERSION}/"
+                                else
+                                TARBALL_LINK="https://repo.percona.com/private/${USERNAME}-${PASSWORD}/qa-test/ps-gated-${PS_VERSION}/"
                                 rm -rf package-testing
                                 git clone https://github.com/kaushikpuneet07/package-testing.git --branch ps-eol --depth 1
                                 cd package-testing/binary-tarball-tests/ps
