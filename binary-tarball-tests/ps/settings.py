17426cac8a2e378b518eeaf4599f8bb07c6c5eb7
Puneet Kaushik <87129594+kaushikpuneet07@users.noreply.github.com>
2024-03-28 09:06:17 +0530

    Updated w.r.t tarball (#657)

--- binary-tarball-tests/ps/settings.py
+++ binary-tarball-tests/ps/settings.py
@@ -169,7 +169,7 @@ ps8x_components = (
 ps8x_files = (
   'lib/libcoredumper.a', 
   'lib/mysqlrouter/private/libmysqlrouter_http.so.1', 'lib/mysqlrouter/private/libmysqlrouter.so.1', 'lib/libmysqlservices.a',
-  'lib/libperconaserverclient.a', 'lib/libperconaserverclient.so.22.1.0' ,'lib/mysql/libjemalloc.so.1',
+  'lib/libperconaserverclient.a', 'lib/libperconaserverclient.so.23.1.0' ,'lib/mysql/libjemalloc.so.1',
   'lib/plugin/ha_rocksdb.so', 'lib/plugin/auth_pam.so', 'lib/plugin/auth_pam_compat.so',
   'lib/plugin/keyring_file.so','lib/plugin/component_binlog_utils_udf.so',
   'lib/plugin/keyring_udf.so', 'lib/plugin/component_keyring_vault.so', 'lib/plugin/component_binlog_utils_udf.so',
@@ -177,8 +177,8 @@ ps8x_files = (
 )
 
 ps8x_symlinks = (
-  ('lib/libperconaserverclient.so.22','lib/libperconaserverclient.so.22.1.0'),
-  ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.22.1.0'),('lib/mysql/libjemalloc.so','lib/mysql/libjemalloc.so.1')
+  ('lib/libperconaserverclient.so.22','lib/libperconaserverclient.so.23.1.0'),
+  ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.23.1.0'),('lib/mysql/libjemalloc.so','lib/mysql/libjemalloc.so.1')
 )
 
 #####
