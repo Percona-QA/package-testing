2fe22b1dc9999356c182ee83c649f8ae901e390b
Puneet Kaushik <puneet.kaushik@percona.com>
2024-07-05 11:56:59 +0530

    update

--- docker-image-tests/percona-mysql-router/tests/test_router_static.py
+++ docker-image-tests/percona-mysql-router/tests/test_router_static.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python3
-import pytest
 import subprocess
-import testinfra
 import time
-from settings import *
 import json
 import os
-import requests
-import docker
+import pytest
+import testinfra
 
-container_name = 'mysql-router-test'
 container_name_mysql_router = 'mysql-router'
-network_name = 'innodbnet1'
+network_name = 'innodbnet'
 docker_tag = os.getenv('ROUTER_VERSION')
 docker_acc = os.getenv('DOCKER_ACC')
 ps_version = os.getenv('PS_VERSION')
-router_docker_image = docker_acc + "/" + "percona-mysql-router" + ":" + docker_tag
-percona_docker_image = docker_acc + "/" + "percona-server" + ":" + ps_version
+router_docker_image = f"{docker_acc}/percona-mysql-router:{docker_tag}"
+percona_docker_image = f"{docker_acc}/percona-server:{ps_version}"
 
 def create_network():
-    subprocess.run(['docker', 'network', 'create', 'innodbnet'])
+    subprocess.run(['docker', 'network', 'create', network_name], check=True)
 
 def create_mysql_config():
     for N in range(1, 5):
@@ -46,9 +42,9 @@ def start_mysql_containers():
             f'--name=mysql{N}',
             f'--hostname=mysql{N}',
             '--net=innodbnet',
-            '-v', f'my{N}.cnf:/etc/my.cnf',
+            '-v', f"{os.getcwd()}/my{N}.cnf:/etc/my.cnf",
             '-e', 'MYSQL_ROOT_PASSWORD=root', percona_docker_image
-        ])
+        ], check=True)
     time.sleep(60)
 
 def create_new_user():
@@ -57,46 +53,46 @@ def create_new_user():
             'docker', 'exec', f'mysql{N}',
             'mysql', '-uroot', '-proot',
             '-e', "CREATE USER 'inno'@'%' IDENTIFIED BY 'inno'; GRANT ALL privileges ON *.* TO 'inno'@'%' with grant option; FLUSH PRIVILEGES;"
-        ])
+        ], check=True)
 
 def verify_new_user():
     for N in range(1, 5):
         subprocess.run([
             'docker', 'exec', f'mysql{N}',
             'mysql', '-uinno', '-pinno',
-            '-e', "SHOW VARIABLES WHERE Variable_name = 'hostname';"
-            '-e', "SELECT user FROM mysql.user where user = 'inno';"
-        ])
+            '-e', "SHOW VARIABLES WHERE Variable_name = 'hostname';",
+            '-e', "SELECT user FROM mysql.user WHERE user = 'inno';"
+        ], check=True)
     time.sleep(30)
 
 def docker_restart():
-    subprocess.run(['docker', 'restart', 'mysql1', 'mysql2', 'mysql3', 'mysql4'])
+    subprocess.run(['docker', 'restart', 'mysql1', 'mysql2', 'mysql3', 'mysql4'], check=True)
     time.sleep(10)
 
 def create_cluster():
     subprocess.run([
         'docker', 'exec', 'mysql1',
         'mysqlsh', '-uinno', '-pinno', '--', 'dba', 'create-cluster', 'testCluster'
-    ])
+    ], check=True)
 
 def add_slave():
     subprocess.run([
         'docker', 'exec', 'mysql1',
         'mysqlsh', '-uinno', '-pinno', '--',
         'cluster', 'add-instance', '--uri=inno@mysql3', '--recoveryMethod=incremental'
-    ])
+    ], check=True)
     time.sleep(10)
     subprocess.run([
         'docker', 'exec', 'mysql1',
         'mysqlsh', '-uinno', '-pinno', '--',
         'cluster', 'add-instance', '--uri=inno@mysql4', '--recoveryMethod=incremental'
-    ])
+    ], check=True)
 
 @pytest.fixture(scope='module')
 def inspect_data():
-    dockerid = subprocess.check_output([
+    docker_id = subprocess.check_output([
         'docker', 'run', '-d',
-        '--name', 'mysql-router',
+        '--name', container_name_mysql_router,
         '--net=innodbnet',
         '-e', 'MYSQL_HOST=mysql1',
         '-e', 'MYSQL_PORT=3306',
@@ -105,7 +101,7 @@ def inspect_data():
         '-e', 'MYSQL_INNODB_CLUSTER_MEMBERS=4',
         router_docker_image
     ]).decode().strip()
-    inspect_data = json.loads(subprocess.check_output(['docker','inspect','mysql-router']))
+    inspect_data = json.loads(subprocess.check_output(['docker','inspect', container_name_mysql_router]))
     yield inspect_data[0]
     subprocess.check_call(['docker', 'rm', '-f', docker_id])
 
@@ -118,30 +114,23 @@ verify_new_user()
 docker_restart()
 create_cluster()
 add_slave()
-inspect_data()
 
 class TestRouterEnvironment:
     def test_mysqlrouter_version(self, host):
         command = "mysqlrouter --version"
         output = host.check_output(command)
-        assert ROUTER_VERSION in output
+        assert docker_tag in output
 
     def test_mysqlsh_version(self, host):
         command = "mysqlsh --version"
         output = host.check_output(command)
-        assert PS_VERSION in output
+        assert ps_version in output
 
     def test_mysqlrouter_directory_permissions(self, host):
         assert host.file('/var/lib/mysqlrouter').user == 'mysql'
         assert host.file('/var/lib/mysqlrouter').group == 'mysql'
         assert oct(host.file('/var/lib/mysqlrouter').mode) == '0o755'
 
-    def test_mysql_user(self, host):
-        assert host.user('mysql').exists
-        assert host.user('mysql').uid == 1001
-        assert host.user('mysql').gid == 1001
-        assert 'mysql' in host.user('mysql').groups
- 
     def test_mysql_user(self, host):
         mysql_user = host.user('mysql')
         print(f"Username: {mysql_user.name}, UID: {mysql_user.uid}")
@@ -158,4 +147,4 @@ class TestRouterEnvironment:
         assert host.file("/etc/mysqlrouter/mysqlrouter.conf").exists
         assert host.file("/etc/mysqlrouter/mysqlrouter.conf").user == "root"
         assert host.file("/etc/mysqlrouter/mysqlrouter.conf").group == "root"
-        assert oct(host.file("/etc/mysqlrouter/mysqlrouter.conf").mode) == "0o644"    
+        assert oct(host.file("/etc/mysqlrouter/mysqlrouter.conf").mode) == "0o644"
