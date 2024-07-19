56e7b357cc19c868dbc38e1c236d908e64f82355
Puneet Kaushik <puneet.kaushik@percona.com>
2024-07-19 13:41:59 +0530

    update

--- binary-tarball-tests/ps/settings.py
+++ binary-tarball-tests/ps/settings.py
@@ -40,7 +40,7 @@ ps80_executables = ps80_binaries + [
 ps80_plugins = (
   ('audit_log','audit_log.so'),('mysql_no_login','mysql_no_login.so'),('validate_password','validate_password.so'),
   ('version_tokens','version_token.so'),('rpl_semi_sync_master','semisync_master.so'),('rpl_semi_sync_slave','semisync_slave.so'),
-  ('group_replication','group_replication.so'),('clone','mysql_clone.so'),('data_masking','data_masking.so'),
+  ('clone','mysql_clone.so'),('data_masking','data_masking.so'),
   ('procfs', 'procfs.so'), ('authentication_ldap_sasl','authentication_ldap_sasl.so')
 )
 ps80_functions = (
