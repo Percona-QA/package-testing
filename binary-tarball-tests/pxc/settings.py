3918274cb8dfa6ba3f5c20e03786e369063fbfa5
puneetkaushik <kaushik.puneet07@gmail.com>
2024-09-07 00:00:03 +0530

    update

--- binary-tarball-tests/pxc/settings.py
+++ binary-tarball-tests/pxc/settings.py
@@ -136,17 +136,9 @@ if glibc_version == '2.35':
     ('lib/libbrotlidec.so', 'lib/private/libbrotlidec.so.1.0.9'), ('lib/libprocps.so', 'lib/private/libprocps.so.8.0.3'),
     ('lib/librtmp.so', 'lib/private/librtmp.so.1'),('lib/libtirpc.so', 'lib/private/libtirpc.so.3.0.0')
   )
-elif glibc_version == '2.34':
-  pxc80_symlinks = (
-    ('lib/libnssutil3.so','lib/private/libnssutil3.so'),
-    ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.21.2.37'),
-    ('lib/libplds4.so','lib/private/libplds4.so'), ('lib/libsasl2.so','lib/private/libsasl2.so.3.0.0'),
-    ('lib/libsmime3.so','lib/private/libsmime3.so'), ('lib/libssl.so','lib/private/libssl.so.1.0.2k'),
-    ('lib/libssl3.so','lib/private/libssl3.so'), ('lib/libtinfo.so','lib/private/libtinfo.so.5.9'),
-  )
 else:
   pxc80_symlinks = (
-    ('lib/libnss3.so','lib/private/libnss3.so'), ('lib/libnssutil3.so','lib/private/libnssutil3.so'),
+    ('lib/libnssutil3.so','lib/private/libnssutil3.so'),
     ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.21.2.37'), ('lib/libplc4.so','lib/private/libplc4.so'),
     ('lib/libplds4.so','lib/private/libplds4.so'), ('lib/libsasl2.so','lib/private/libsasl2.so.3.0.0'),
     ('lib/libsmime3.so','lib/private/libsmime3.so'), ('lib/libssl.so','lib/private/libssl.so.1.0.2k'),
