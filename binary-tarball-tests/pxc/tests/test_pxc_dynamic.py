3150d1c683e2ab318024dc15c629b0b134425e70
kaushikpuneet07 <puneet.kaushik@percona.com>
2024-11-26 17:22:10 +0530

    update

--- binary-tarball-tests/pxc/tests/test_pxc_dynamic.py
+++ binary-tarball-tests/pxc/tests/test_pxc_dynamic.py
@@ -21,6 +21,10 @@ def test_install_plugin(mysql_server):
     for plugin in pxc_plugins:
         mysql_server.install_plugin(plugin[0], plugin[1])
 
+def test_install_components(mysql_server):
+    for plugin in pxc_components:
+        mysql_server.install_component(component[0])
+
 def test_cluster_size(mysql_server):
     output = mysql_server.run_query('SHOW STATUS LIKE "wsrep_cluster_size";')
     assert output.split('\t')[1].strip() == "3"
