ffd2b328ef24a3cb96fad006fe0a00cdeec47ae1
Puneet Kaushik <87129594+kaushikpuneet07@users.noreply.github.com>
2024-12-12 15:32:25 +0530

    Fix myrocks test for ARM processors (#812)

--- scripts/ps_myrocks_test.sh
+++ scripts/ps_myrocks_test.sh
@@ -2,15 +2,25 @@
 
 LOGFILE=""
 
-if [ -f /var/lib/mysql/.rocksdb/LOG ]; then
+# Check for RocksDB log file
+if [ -f /var/lib/mysql/.rocksdb/LOG" ]; then
   LOGFILE="/var/lib/mysql/.rocksdb/LOG"
 else
   echo "Can't find RocksDB LOG file!"
   exit 1
 fi
 
-# check if FastCRC32 is enabled
-fastcrc=$(grep -c "Fast CRC32 supported: Supported on x86" ${LOGFILE})
+# Check CPU architecture
+arch=$(uname -m)
+if [ "${arch}" != "x86_64" ]; then
+  echo "Unsupported architecture: ${arch}. Skipping CRC test."
+  exit 0
+else
+  echo "Architecture is x86_64. Proceeding with CRC test."
+fi
+
+# Check if FastCRC32 is enabled
+fastcrc=$(grep -c "Fast CRC32 supported: Supported on x86" "${LOGFILE}")
 if [ ${fastcrc} -eq 0 ]; then
   echo "Fast CRC32 doesn't seem to be enabled."
   exit 1
