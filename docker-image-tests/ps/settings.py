aaadf37a9d81dd0df71faa9323ac15efad51e5b8
Puneet Kaushik <87129594+kaushikpuneet07@users.noreply.github.com>
2024-12-20 20:31:00 +0530

    fix docker job (#828)

--- docker-image-tests/ps/settings.py
+++ docker-image-tests/ps/settings.py
@@ -29,8 +29,8 @@ ps8x_binaries = (
   '/usr/sbin/mysqld-debug', '/usr/bin/mysqldump', '/usr/bin/mysqldumpslow', '/usr/bin/mysqlimport',
   '/usr/bin/mysqlshow', '/usr/bin/mysqlslap', '/usr/bin/mysqlcheck', '/usr/bin/mysql_config_editor', '/usr/bin/mysql_config',
   '/usr/bin/mysql_config-64', '/usr/bin/mysql_secure_installation', '/usr/bin/find', '/usr/bin/kill',
-  '/usr/bin/hostname', '/usr/bin/gunzip', '/usr/bin/my_print_defaults', '/usr/bin/cat', '/usr/bin/mysql_tzinfo_to_sql',
-  '/usr/bin/grep', '/usr/bin/cut', '/usr/bin/tail', '/usr/bin/sed', '/usr/bin/gawk', '/usr/bin/mysqlsh'
+  '/usr/bin/hostname', '/usr/bin/gunzip', '/usr/bin/my_print_defaults', '/usr/bin/mysql_tzinfo_to_sql',
+  '/usr/bin/grep', '/usr/bin/sed', '/usr/bin/gawk', '/usr/bin/mysqlsh'
 )
 ps8x_plugins = (
   ('mysql_no_login','mysql_no_login.so'),('validate_password','validate_password.so'),
