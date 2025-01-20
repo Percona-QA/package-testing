e20d98fef8021a475b52d63fc82e02468fe9a261
kaushikpuneet07 <puneet.kaushik@percona.com>
2025-01-20 16:52:55 +0530

    update

--- binary-tarball-tests/pxc/settings.py
+++ binary-tarball-tests/pxc/settings.py
@@ -67,7 +67,7 @@ pxc8x_files = (
 if glibc_version == '2.35':
   pxc8x_symlinks = (
     ('lib/libcrypto.so', 'lib/private/libcrypto.so.3'),('lib/libgcrypt.so', 'lib/private/libgcrypt.so.20.3.4',),
-    ('lib/libperconaserverclient.so', 'lib/libperconaserverclient.so.23.0.0'),('lib/libsasl2.so', 'lib/private/libsasl2.so.2.0.25'),
+    ('lib/libperconaserverclient.so', 'lib/libperconaserverclient.so.24.0.0'),('lib/libsasl2.so', 'lib/private/libsasl2.so.2.0.25'),
     ('lib/libssl.so', 'lib/private/libssl.so.3'),('lib/libtinfo.so', 'lib/private/libtinfo.so.6.3'),
     ('lib/libaio.so','lib/private/libaio.so.1.0.1'),('lib/libbrotlicommon.so', 'lib/private/libbrotlicommon.so.1.0.9'),
     ('lib/libbrotlidec.so', 'lib/private/libbrotlidec.so.1.0.9'), ('lib/libprocps.so', 'lib/private/libprocps.so.8.0.3'),
@@ -79,13 +79,13 @@ else:
 
     ('lib/libgcrypt.so','lib/private/libgcrypt.so.11.8.2'), ('lib/libnspr4.so','lib/private/libnspr4.so'),
     ('lib/libnss3.so','lib/private/libnss3.so'), ('lib/libnssutil3.so','lib/private/libnssutil3.so'),
-    ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.23.0.0'), ('lib/libplc4.so','lib/private/libplc4.so'),
+    ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.24.0.0'), ('lib/libplc4.so','lib/private/libplc4.so'),
     ('lib/libplds4.so','lib/private/libplds4.so'), ('lib/libsasl2.so','lib/private/libsasl2.so.3.0.0'),
     ('lib/libsmime3.so','lib/private/libsmime3.so'), ('lib/libssl.so','lib/private/libssl.so.1.0.2k'),
     ('lib/libssl3.so','lib/private/libssl3.so'), ('lib/libtinfo.so','lib/private/libtinfo.so.5.9'),
   )
 pxc8x_components = (
-  ('file://component_encryption_udf'),('file://component_keyring_kmip'),('file://component_keyring_kms'),('file://component_masking_functions'),('file://component_binlog_utils_udf'),('file://component_percona_udf'),('file://component_audit_log_filter'),('file://component_keyring_vault'),('file://component_binlog_uts_udf')
+  ('file://component_encryption_udf'),('file://component_keyring_kmip'),('file://component_keyring_kms'),('file://component_masking_functions'),('file://component_binlog_utils_udf'),('file://component_percona_udf'),('file://component_audit_log_filter'),('file://component_keyring_vault')
 )
 
 # 8.0
