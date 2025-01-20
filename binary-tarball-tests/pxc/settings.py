250ab13b3af81d56a996cb580134805c155bbbd9
kaushikpuneet07 <puneet.kaushik@percona.com>
2025-01-20 20:37:00 +0530

    update

--- binary-tarball-tests/pxc/settings.py
+++ binary-tarball-tests/pxc/settings.py
@@ -60,19 +60,17 @@ pxc8x_files = (
 if glibc_version == '2.35':
   pxc8x_symlinks = (
     ('lib/libperconaserverclient.so', 'lib/libperconaserverclient.so.24.0.3'),('lib/libsasl2.so', 'lib/private/libsasl2.so.2.0.25'),
-    ('lib/libssl.so', 'lib/private/libssl.so.3'),('lib/libtinfo.so', 'lib/private/libtinfo.so.6.3'),
+    ('lib/libtinfo.so', 'lib/private/libtinfo.so.6.3'),
     ('lib/libaio.so','lib/private/libaio.so.1.0.1'),('lib/libbrotlicommon.so', 'lib/private/libbrotlicommon.so.1.0.9'),
     ('lib/libbrotlidec.so', 'lib/private/libbrotlidec.so.1.0.9'), ('lib/libprocps.so', 'lib/private/libprocps.so.8.0.3'),
-    ('lib/librtmp.so', 'lib/private/librtmp.so.1'),('lib/libtirpc.so', 'lib/private/libtirpc.so.3.0.0')
+    ('lib/librtmp.so', 'lib/private/librtmp.so.1')
   )
 else:
   pxc8x_symlinks = (
-    ('lib/libgcrypt.so','lib/private/libgcrypt.so.20.3.4'), ('lib/libnspr4.so','lib/private/libnspr4.so'),
-    ('lib/libnss3.so','lib/private/libnss3.so'), ('lib/libnssutil3.so','lib/private/libnssutil3.so'),
-    ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.24.0.3'), ('lib/libplc4.so','lib/private/libplc4.so'),
-    ('lib/libplds4.so','lib/private/libplds4.so'), ('lib/libsasl2.so','lib/private/libsasl2.so.3.0.0'),
-    ('lib/libsmime3.so','lib/private/libsmime3.so'), ('lib/libssl.so','lib/private/libssl.so.1.0.2k'),
-    ('lib/libssl3.so','lib/private/libssl3.so'), ('lib/libtinfo.so','lib/private/libtinfo.so.6.3'),
+    ('lib/libgcrypt.so','lib/private/libgcrypt.so.20.3.4'),
+    ('lib/libperconaserverclient.so','lib/libperconaserverclient.so.24.0.3'),
+    ('lib/libplds4.so','lib/private/libplds4.so'), ('lib/libsasl2.so','lib/private/libsasl2.so.2.0.25'),
+    ('lib/libtinfo.so','lib/private/libtinfo.so.6.3'),
   )
 pxc8x_components = (
   ('file://component_encryption_udf'),('file://component_keyring_kmip'),('file://component_keyring_kms'),('file://component_masking_functions'),('file://component_binlog_utils_udf'),('file://component_percona_udf'),('file://component_audit_log_filter'),('file://component_keyring_vault')
