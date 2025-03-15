85998a6c65a1b6fb4773101966a9cb35012d60ef
kaushikpuneet07 <puneet.kaushik@percona.com>
2025-03-15 20:38:54 +0530

    update

--- molecule/pdmysql/tests/test_pdps_sources_orch.py
+++ molecule/pdmysql/tests/test_pdps_sources_orch.py
@@ -1,33 +1,20 @@
 import pytest
-from packaging import version
 from .settings import *
+from packaging import version
+
+VERSION = os.environ.get("VERSION")
+
 
 @pytest.mark.orch_source
 def test_integration(host):
     with host.sudo():
         dist = host.system_info.distribution
-        # Fetch MySQL version dynamically
-        version_command = "mysql --version"
-        result = host.run(version_command)
-        assert result.rc == 0, f"Failed to get MySQL version: {result.stderr}"
-
-        mysql_version = result.stdout.split()[4]  # Extract version part
-        print(f"MySQL version: {mysql_version}")
-
-        # Check the MySQL version and decide which authentication method to use
-        if version.parse(mysql_version) > version.parse("8.0.0") and version.parse(mysql_version) < version.parse("8.1.0"):
+        if dist.lower() not in RHEL_DISTS and version.parse(VERSION) > version.parse("8.0.0") and version.parse(VERSION) < version.parse("8.1.0"):
+            command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';\""
+        elif dist.lower() not in RHEL_DISTS and version.parse(VERSION) >= version.parse("8.1.0"):
             command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'root';\""
-        elif version.parse(mysql_version) >= version.parse("8.1.0"):
-            command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';\""
-        else:
-            # For MySQL 8.0.0 or earlier, you can leave it as is (or add any other adjustments)
-            command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';\""
-
-        # Run the command to alter the root user's authentication
-        result = host.run(command)
-        assert result.rc == 0, f"Failed to alter MySQL root user: {result.stderr}"
-
-        # Now, run the rest of the test
+            result = host.run(command)
+            assert result.rc == 0, (result.stderr, result.stdout)
         test_cmd = "cd /root/orchestrator/ && ./tests/integration/test.sh mysql"
         test = host.run(test_cmd)
         print(test.stdout)
