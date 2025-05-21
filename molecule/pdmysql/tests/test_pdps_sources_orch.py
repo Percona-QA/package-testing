76cadb3b1f33a8ecf67ec9ac2aa8b7a9aa8d1e63
kaushikpuneet07 <puneet.kaushik@percona.com>
2025-05-21 10:47:49 +0530

    update

--- molecule/pdmysql/tests/test_pdps_sources_orch.py
+++ molecule/pdmysql/tests/test_pdps_sources_orch.py
@@ -9,9 +9,10 @@ VERSION = os.environ.get("VERSION")
 def test_integration(host):
     with host.sudo():
         dist = host.system_info.distribution
-        command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'root';\""
-        result = host.run(command)
-        assert result.rc == 0, (result.stderr, result.stdout)
+        if dist.lower() not in RHEL_DISTS:
+            command = "mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'root';\""
+            result = host.run(command)
+            assert result.rc == 0, (result.stderr, result.stdout)
         test_cmd = "cd /root/orchestrator/ && ./tests/integration/test.sh mysql"
         test = host.run(test_cmd)
         print(test.stdout)
