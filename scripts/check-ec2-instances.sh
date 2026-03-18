75178fcff5b16199a36b6381a7177a86a8e4052d
Yash <46398416+panchal-yash@users.noreply.github.com>
2026-03-18 17:56:13 +0530

    pt instance deletion fixes 1 (#1098)

--- scripts/check-ec2-instances.sh
+++ scripts/check-ec2-instances.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
-#
-## Calculate the threshold date (1 day ago)
-awsRegions=("us-west-1" "us-west-2" "us-east-1" "us-east-2" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "eu-central-1")
 
+awsRegions=("us-west-1" "us-west-2" "us-east-1" "us-east-2" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "eu-central-1")
 
 checkec2-qa(){
            threshold_date=$(date -d "$2 day ago" +%Y-%m-%d)
-           aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:job-name,Values=*pxb*,*package-testing*,*ps*,*pxc*,*pbm*,*pdmdb*,*orchestrator_docker*,*toolkit*,*pt*" --query "Reservations[].Instances[?LaunchTime<='${threshold_date}'].[Tags[?Key=='Name'].Value[],Tags[?Key=='job-name'].Value[], InstanceId, LaunchTime]" --output yaml --region $1 | sed -e 's/\- \[\]//g' -e '/^$/d'
-
+            aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[].Instances[?LaunchTime<='"${threshold_date}"' && (Tags[?Key=='iit-billing-tag' && (contains(Value,'package-testing') || contains(Value,'molecule'))] || Tags[?Key=='job-name' && (contains(Value,'pxb') || contains(Value,'package-testing') || contains(Value,'ps') || contains(Value,'pxc') || contains(Value,'pbm') || contains(Value,'pdmdb') || contains(Value,'orchestrator_docker') || contains(Value,'toolkit') || contains(Value,'pt') || contains(Value,'jenkins-pg-worker') || contains(Value,'jenkins-pg-molecule-rhel'))])].[Tags[?Key=='Name'].Value[],Tags[?Key=='job-name'].Value[], InstanceId, LaunchTime]" --output yaml --region $1 | sed -e 's/\- \[\]//g' -e '/^$/d'
     }
 
 checkec2-all(){
            threshold_date=$(date -d "$2 day ago" +%Y-%m-%d)
            aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[].Instances[?LaunchTime<='${threshold_date}'].[Tags[?Key=='Name'].Value[],Tags[?Key=='job-name'].Value[], InstanceId, LaunchTime]" --output yaml --region $1 | sed -e 's/\- \[\]//g' -e '/^$/d'
-
     }
 
-#
 days="2"
 
 for region in "${awsRegions[@]}"
 do
-#
-#
+
 serversall=$(checkec2-all "$region" "$days" | /opt/yq 'length')
 serversqa=$(checkec2-qa "$region" "$days" | /opt/yq 'length')
-
 
 echo "--------------Region $region has $serversall servers running since past $days Days-----------------" >> OUTPUT-ALL.txt
 checkec2-all "$region" "$days"  >> OUTPUT-ALL.txt
