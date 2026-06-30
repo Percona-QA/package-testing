b0851d4bfccab4601de9124292d42391cec70c7e
kaushikpuneet07 <puneet.kaushik@percona.com>
2026-06-30 13:43:37 +0530

    update

--- docker-image-tests/ps-arm/settings.py
+++ docker-image-tests/ps-arm/settings.py
@@ -16,6 +16,34 @@ docker_product = 'percona-server'
 docker_tag = ps_version
 docker_image = docker_acc + "/" + docker_product + ":" + docker_tag
 
+# 9.7
+ps97_packages = (
+  'percona-server-client', 'percona-server-rocksdb', 'percona-server-server',
+  'percona-server-shared'
+)
+ps97_binaries = (
+  '/usr/bin/mysql', '/usr/sbin/mysqld', '/usr/bin/ps-admin', '/usr/bin/mysqladmin', '/usr/bin/mysqlbinlog',
+  '/usr/sbin/mysqld-debug', '/usr/bin/mysqldump', '/usr/bin/mysqldumpslow', '/usr/bin/mysqlimport',
+  '/usr/bin/mysqlshow', '/usr/bin/mysqlslap', '/usr/bin/mysqlcheck', '/usr/bin/mysql_config_editor', '/usr/bin/mysql_config',
+  '/usr/bin/mysql_secure_installation', '/usr/bin/sed', '/usr/bin/find', '/usr/bin/kill'
+  '/usr/bin/hostname', '/usr/bin/gunzip', '/usr/bin/my_print_defaults', '/usr/bin/cat', '/usr/bin/mysql_tzinfo_to_sql',
+  '/usr/bin/grep', '/usr/bin/cut', '/usr/bin/tail'
+)
+ps97_plugins = (
+  ('mysql_no_login','mysql_no_login.so'),('validate_password','validate_password.so'),
+  ('version_tokens','version_token.so'),('rpl_semi_sync_master','semisync_master.so'),('rpl_semi_sync_slave','semisync_slave.so'),
+  ('group_replication','group_replication.so'),('clone','mysql_clone.so')
+)
+ps97_components = (
+  ('file://component_encryption_udf'),('file://component_keyring_kmip'),('file://component_keyring_kms'),('file://component_masking_functions'),('file://component_binlog_utils_udf'),('file://component_percona_udf'),('file://component_audit_log_filter'),('file://component_keyring_vault')
+)
+ps97_functions = (
+  ('version_tokens_set', 'version_token.so', 'STRING'),('version_tokens_show', 'version_token.so', 'STRING'),('version_tokens_edit', 'version_token.so', 'STRING'),
+  ('version_tokens_delete', 'version_token.so', 'STRING'),('version_tokens_lock_shared', 'version_token.so', 'INT'),('version_tokens_lock_exclusive', 'version_token.so', 'INT'),
+  ('version_tokens_unlock', 'version_token.so', 'INT'),('service_get_read_locks', 'locking_service.so', 'INT'),('service_get_write_locks', 'locking_service.so', 'INT'), ('service_release_locks', 'locking_service.so', 'INT')
+)
+
+
 # 8.X
 ps8x_packages = (
   'percona-server-client', 'percona-server-rocksdb', 'percona-server-server',
@@ -118,7 +146,13 @@ ps56_functions = (
 )
 #####
 
-if  re.match(r'^8\.[1-9]$', ps_version_major):
+if  re.match(r'^9\.[1-9]$', ps_version_major):
+    ps_packages = ps97_packages
+    ps_binaries = ps97_binaries
+    ps_plugins = ps97_plugins
+    ps_functions = ps97_functions
+    ps_components = ps97_components
+elif  re.match(r'^8\.[1-9]$', ps_version_major):
     ps_packages = ps8x_packages
     ps_binaries = ps8x_binaries
     ps_plugins = ps8x_plugins
